d8530fb9623a5e3f409fdbd6dead2654218e9966
Jean THOMAS <git0@pub.jeanthomas.me>
2018-01-31 22:02:56 +0100

    Adding command info when wrong argument number in horizon-pool

--- src/pool-util/util_main.cpp
+++ src/pool-util/util_main.cpp
@@ -91,38 +91,64 @@ int main(int c_argc, char *c_argv[])
     }
     auto pool_base_path = Glib::getenv("HORIZON_POOL");
 
-    if (argv.size() >= 2 && argv.at(1) == "create-unit" || argv.at(1) == "edit-unit") {
-        const auto &filename = argv.at(2);
-        horizon::Unit unit(horizon::UUID::random());
-        if (argv.at(1) == "edit-unit") {
-            unit = horizon::Unit::new_from_file(filename);
-        }
-        YAML::Emitter em;
-        unit.serialize_yaml(em);
-        auto node = edit_yaml(em);
-        horizon::Unit unit_new(node["uuid"].as<std::string>(), node);
-        auto j = unit_new.serialize();
-        horizon::save_json_to_file(filename, j);
-    }
-    else if (argv.size() >= 4 && argv.at(1) == "create-symbol") {
-        const auto &filename_sym = argv.at(2);
-        const auto &filename_unit = argv.at(3);
-
-        horizon::Symbol sym(horizon::UUID::random());
-        auto unit = horizon::Unit::new_from_file(filename_unit);
-        sym.unit = &unit;
-        horizon::save_json_to_file(filename_sym, sym.serialize());
+    if (argv.size() <= 1) {
+        std::cout << "Usage: " << argv.at(0) << " <command> args" << std::endl << std::endl;
+        std::cout << "Available commands :" << std::endl;
+        std::cout << "\tcreate-unit <unit file>" << std::endl;
+        std::cout << "\tedit-unit <unit file>" << std::endl;
+        std::cout << "\tcreate-symbol <symbol file> <unit file>" << std::endl;
+        std::cout << "\tcreate-entity <entity file> [unit file 1] ..." << std::endl;
+        std::cout << "\tedit-entity <entity file>" << std::endl;
+        std::cout << "\tcreate-package <package folder>" << std::endl;
+        std::cout << "\tcreate-padstack <padstack file>" << std::endl;
+        std::cout << "\tupdate" << std::endl;
+        return 0;
     }
 
-    else if (argv.size() >= 3 && argv.at(1) == "create-entity" || argv.at(1) == "edit-entity") {
-        const auto &filename = argv.at(2);
-        horizon::Entity entity(horizon::UUID::random());
-        horizon::Pool pool(pool_base_path);
-        if (argv.at(1) == "edit-entity") {
-            entity = horizon::Entity::new_from_file(filename, pool);
+    if (argv.at(1) == "create-unit" || argv.at(1) == "edit-unit") {
+        if (argv.size() >= 3) {
+            const auto &filename = argv.at(2);
+            horizon::Unit unit(horizon::UUID::random());
+            if (argv.at(1) == "edit-unit") {
+                unit = horizon::Unit::new_from_file(filename);
+            }
+            YAML::Emitter em;
+            unit.serialize_yaml(em);
+            auto node = edit_yaml(em);
+            horizon::Unit unit_new(node["uuid"].as<std::string>(), node);
+            auto j = unit_new.serialize();
+            horizon::save_json_to_file(filename, j);
+        } else {
+            if (argv.at(1) == "create-unit") {
+                std::cout << "Usage: " << argv.at(0) << " create-unit <unit file>" << std::endl;
+            } else {
+                std::cout << "Usage: " << argv.at(0) << " edit-unit <unit file>" << std::endl;
+            }
         }
-        else {
-            if (argv.size() > 3) {
+    }
+
+    else if (argv.at(1) == "create-symbol") {
+        if (argv.size() >= 4) {
+            const auto &filename_sym = argv.at(2);
+            const auto &filename_unit = argv.at(3);
+
+            horizon::Symbol sym(horizon::UUID::random());
+            auto unit = horizon::Unit::new_from_file(filename_unit);
+            sym.unit = &unit;
+            horizon::save_json_to_file(filename_sym, sym.serialize());
+        } else {
+            std::cout << "Usage: " << argv.at(0) << " create-symbol <symbol file> <unit file>" << std::endl;
+        }
+    }
+
+    else if (argv.at(1) == "create-entity" || argv.at(1) == "edit-entity") {
+        if (argv.size() >= 3) {
+            const auto &filename = argv.at(2);
+            horizon::Entity entity(horizon::UUID::random());
+            horizon::Pool pool(pool_base_path);
+            if (argv.at(1) == "edit-entity") {
+                entity = horizon::Entity::new_from_file(filename, pool);
+            } else if (argv.size() > 3) {
                 for (auto it = argv.cbegin() + 3; it < argv.cend(); it++) {
                     auto unit = horizon::Unit::new_from_file(*it);
                     auto uu = horizon::UUID::random();
@@ -131,34 +157,48 @@ int main(int c_argc, char *c_argv[])
                     gate.name = "Main";
                 }
             }
+            YAML::Emitter em;
+            entity.serialize_yaml(em);
+            auto node = edit_yaml(em);
+            horizon::Entity entity_new(node["uuid"].as<std::string>(), node, pool);
+            auto j = entity_new.serialize();
+            horizon::save_json_to_file(filename, j);
+        } else {
+            if (argv.at(1) == "create-entity") {
+                std::cout << "Usage: " << argv.at(0) << " create-entity <entity file> [unit file 1] ..." << std::endl;
+            } else {
+                std::cout << "Usage: " << argv.at(0) << " edit-entity <entity file>" << std::endl;
+            }
         }
-        YAML::Emitter em;
-        entity.serialize_yaml(em);
-        auto node = edit_yaml(em);
-        horizon::Entity entity_new(node["uuid"].as<std::string>(), node, pool);
-        auto j = entity_new.serialize();
-        horizon::save_json_to_file(filename, j);
     }
 
-    else if (argv.size() >= 3 && argv.at(1) == "create-package") {
-        auto &base_path = argv.at(2);
-        {
-            auto fi = Gio::File::create_for_path(Glib::build_filename(base_path, "padstacks"));
-            fi->make_directory_with_parents();
+    else if (argv.at(1) == "create-package") {
+        if (argv.size() >= 3) {
+            auto &base_path = argv.at(2);
+            {
+                auto fi = Gio::File::create_for_path(Glib::build_filename(base_path, "padstacks"));
+                fi->make_directory_with_parents();
+            }
+            auto pkg_filename = Glib::build_filename(base_path, "package.json");
+            horizon::Package pkg(horizon::UUID::random());
+            auto j = pkg.serialize();
+            horizon::save_json_to_file(pkg_filename, j);
+        } else {
+            std::cout << "Usage: " << argv.at(0) << " create-package <package folder>" << std::endl;
         }
-        auto pkg_filename = Glib::build_filename(base_path, "package.json");
-        horizon::Package pkg(horizon::UUID::random());
-        auto j = pkg.serialize();
-        horizon::save_json_to_file(pkg_filename, j);
     }
 
-    else if (argv.size() >= 3 && argv.at(1) == "create-padstack") {
-        horizon::Padstack ps(horizon::UUID::random());
-        auto j = ps.serialize();
-        horizon::save_json_to_file(argv.at(2), j);
+    else if (argv.at(1) == "create-padstack") {
+        if (argv.size() >= 3) {
+            horizon::Padstack ps(horizon::UUID::random());
+            auto j = ps.serialize();
+            horizon::save_json_to_file(argv.at(2), j);
+        } else {
+            std::cout << "Usage: " << argv.at(0) << " create-padstack <padstack file>" << std::endl;
+        }
     }
 
-    else if (argv.size() >= 2 && argv.at(1) == "update") {
+    else if (argv.at(1) == "update") {
         horizon::pool_update(pool_base_path, status_cb);
     }
 
