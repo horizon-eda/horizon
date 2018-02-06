0da36765b3ba19462e7160ccee7f0843594b231e
Jean THOMAS <pub0@github.jeanthomas.me>
2018-02-06 18:50:54 +0100

    Check argv size (pool-util/prj-util) (#83)
    * Add argv size checks in util_main.cpp
    * Add argv size checks in pool-util/util_main.cpp
    * Add command info when wrong argument number in horizon-prj
    * Add command info when wrong argument number in horizon-pool

--- src/prj-util/util_main.cpp
+++ src/prj-util/util_main.cpp
@@ -42,24 +42,45 @@ int main(int c_argc, char *c_argv[])
     }
     auto pool_base_path = Glib::getenv("HORIZON_POOL");
 
+    if (argv.size() <= 1) {
+        std::cout << "Usage: " << argv.at(0) << " <command> args" << std::endl << std::endl;
+        std::cout << "Available commands :" << std::endl;
+        std::cout << "\tcreate-block <block file> [block name]" << std::endl;
+        std::cout << "\tcreate-schematic <block file> <schematic file>" << std::endl;
+        std::cout << "\tcreate-board <block file> <board file>" << std::endl;
+        return 0;
+    }
+
     if (argv.at(1) == "create-block") {
-        horizon::Block block(horizon::UUID::random());
-        if (argv.size() >= 4) {
-            block.name = argv.at(3);
+        if (argv.size() >= 3) {
+            horizon::Block block(horizon::UUID::random());
+            if (argv.size() >= 4) {
+                block.name = argv.at(3);
+            }
+            save_json_to_file(argv.at(2), block.serialize());
+        } else {
+            std::cout << "Usage: " << argv.at(0) << " create-block <block file> [block name]" << std::endl;
         }
-        save_json_to_file(argv.at(2), block.serialize());
     }
     else if (argv.at(1) == "create-schematic") {
-        horizon::Pool pool(pool_base_path);
-        auto block = horizon::Block::new_from_file(argv.at(3), pool);
-        horizon::Schematic sch(horizon::UUID::random(), block);
-        save_json_to_file(argv.at(2), sch.serialize());
+        if (argv.size() >= 4) {
+            horizon::Pool pool(pool_base_path);
+            auto block = horizon::Block::new_from_file(argv.at(3), pool);
+            horizon::Schematic sch(horizon::UUID::random(), block);
+            save_json_to_file(argv.at(2), sch.serialize());
+        } else {
+            std::cout << "Usage: " << argv.at(0) << " create-schematic <block file> <schematic file>" << std::endl;
+        }
     }
     else if (argv.at(1) == "create-board") {
-        horizon::Pool pool(pool_base_path);
-        auto block = horizon::Block::new_from_file(argv.at(3), pool);
-        horizon::Board brd(horizon::UUID::random(), block);
-        save_json_to_file(argv.at(2), brd.serialize());
+        if (argv.size() >= 4) {
+            horizon::Pool pool(pool_base_path);
+            auto block = horizon::Block::new_from_file(argv.at(3), pool);
+            horizon::Board brd(horizon::UUID::random(), block);
+            save_json_to_file(argv.at(2), brd.serialize());
+        } else {
+            std::cout << "Usage: " << argv.at(0) << " create-board <block file> <board file>" << std::endl;
+        }
     }
 
     return 0;
