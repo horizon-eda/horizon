60776c9f67b32f75cbbc0d84fdc88b7ba991c769
Jean THOMAS <git0@pub.jeanthomas.me>
2018-02-07 20:30:07 +0100

    Fix indentations and bugs in run_clangformat

--- scripts/run_clangformat.sh
+++ scripts/run_clangformat.sh
@@ -5,27 +5,25 @@ CLANG_FORMAT=$(command -v clang-format)
 CLANG_FORMAT5=$(command -v clang-format-5.0)
 
 if [ ! -z $CLANG_FORMAT5 ]; then
-	# clang-format-5.0 exists
-	CLANG_BIN=$CLANG_FORMAT5
-  echo "Using clang-format-5.0 command, version 5 detected"
+    # clang-format-5.0 exists
+    CLANG_BIN=$CLANG_FORMAT5
+    echo "Using clang-format-5.0 command, version 5 detected"
 elif [ ! -z $CLANG_FORMAT ]; then
-  CLANG_FORMAT_VERSION="$(clang-format -version | cut -d " " -f 3 | cut -d "." -f 1)"
+    CLANG_FORMAT_VERSION="$(clang-format -version | cut -d " " -f 3 | cut -d "." -f 1)"
 
-  # Check version
-  if [ "$CLANG_FORMAT_VERSION" = "5" ]; then
-	  # clang-format is 5.0
-	  CLANG_BIN=$CLANG_FORMAT
-    echo "Using clang-format command, version 5 detected"
-  fi
+    # Check version
+    if [ "$CLANG_FORMAT_VERSION" = "5" ]; then
+	# clang-format is 5.0
+	CLANG_BIN=$CLANG_FORMAT
+        echo "Using clang-format command, version 5 detected"
+    else
+        echo "clang-format-5.0 was not found, and clang-format was not the correct version"
+	echo "Version was $CLANG_FORMAT_VERSION"
+    fi
 else
-  # error messages
-	if [ -z $CLANG_FORMAT ]; then
-		echo "clang-format was not found"
-	else
-		echo "clang-format-5.0 was not found, and clang-format was not the correct version"
-		echo "Version was $CLANG_FORMAT_VERSION"
-	fi
-	exit -1
+    # error messages
+    echo "clang-format was not found"
+    exit -1
 fi
 
 # Search using the binary found
