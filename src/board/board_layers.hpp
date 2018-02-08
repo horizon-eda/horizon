71d8427630d873f72f5eefe61491146cd3483f76
Lukas K <lu@0x83.eu>
2018-02-08 02:38:46 +0100

    use enum for board layers

--- src/board/board_layers.hpp
+++ src/board/board_layers.hpp
@@ -3,25 +3,27 @@
 namespace horizon {
 class BoardLayers {
 public:
-    static const int L_OUTLINE = 100;
-    static const int TOP_COURTYARD = 60;
-    static const int TOP_ASSEMBLY = 50;
-    static const int TOP_PACKAGE = 40;
-    static const int TOP_PASTE = 30;
-    static const int TOP_SILKSCREEN = 20;
-    static const int TOP_MASK = 10;
-    static const int TOP_COPPER = 0;
-    static const int IN1_COPPER = -1;
-    static const int IN2_COPPER = -2;
-    static const int IN3_COPPER = -3;
-    static const int IN4_COPPER = -4;
-    static const int BOTTOM_COPPER = -100;
-    static const int BOTTOM_MASK = -110;
-    static const int BOTTOM_SILKSCREEN = -120;
-    static const int BOTTOM_PASTE = -130;
-    static const int BOTTOM_PACKAGE = -140;
-    static const int BOTTOM_ASSEMBLY = -150;
-    static const int BOTTOM_COURTYARD = -160;
+    enum Layer {
+        L_OUTLINE = 100,
+        TOP_COURTYARD = 60,
+        TOP_ASSEMBLY = 50,
+        TOP_PACKAGE = 40,
+        TOP_PASTE = 30,
+        TOP_SILKSCREEN = 20,
+        TOP_MASK = 10,
+        TOP_COPPER = 0,
+        IN1_COPPER = -1,
+        IN2_COPPER = -2,
+        IN3_COPPER = -3,
+        IN4_COPPER = -4,
+        BOTTOM_COPPER = -100,
+        BOTTOM_MASK = -110,
+        BOTTOM_SILKSCREEN = -120,
+        BOTTOM_PASTE = -130,
+        BOTTOM_PACKAGE = -140,
+        BOTTOM_ASSEMBLY = -150,
+        BOTTOM_COURTYARD = -160,
+    };
 
     static bool is_copper(int l)
     {
