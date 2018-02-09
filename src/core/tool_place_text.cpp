f655f68bc44b772f484b783f50f5ee64eb10975c
Jean THOMAS <pub0@github.jeanthomas.me>
2018-02-09 01:39:11 +0100

    Ask for text before entering text tool (#107)
    * Ask for text before entering text tool

--- src/core/tool_place_text.cpp
+++ src/core/tool_place_text.cpp
@@ -18,24 +18,37 @@ ToolResponse ToolPlaceText::begin(const ToolArgs &args)
     std::cout << "tool place text\n";
 
     temp = core.r->insert_text(UUID::random());
-    temp->text = "TEXT";
     temp->layer = args.work_layer;
     temp->placement.shift = args.coords;
     imp->tool_bar_set_tip(
-            "<b>LMB:</b>place text <b>RMB:</b>delete current text and finish "
-            "<b>space:</b>enter text");
+            "<b>LMB:</b>place text <b>RMB:</b>finish "
+            "<b>space:</b>change text");
+
+    auto r = imp->dialogs.ask_datum_string("Enter text", temp->text);
+    if (r.first) {
+        temp->text = r.second;
+    }
+    else {
+        core.r->delete_text(temp->uuid);
+        core.r->selection.clear();
+        return ToolResponse::end();
+    }
+
     return ToolResponse();
 }
 ToolResponse ToolPlaceText::update(const ToolArgs &args)
 {
+    std::string *text;
+
     if (args.type == ToolEventType::MOVE) {
         temp->placement.shift = args.coords;
     }
     else if (args.type == ToolEventType::CLICK) {
         if (args.button == 1) {
+            text = &temp->text;
             texts_placed.push_front(temp);
             temp = core.r->insert_text(UUID::random());
-            temp->text = "TEXT";
+            temp->text = *text;
             temp->layer = args.work_layer;
             temp->placement.shift = args.coords;
         }
@@ -55,8 +68,9 @@ ToolResponse ToolPlaceText::update(const ToolArgs &args)
     else if (args.type == ToolEventType::KEY) {
         if (args.key == GDK_KEY_space) {
             auto r = imp->dialogs.ask_datum_string("Enter text", temp->text);
-            if (r.first)
+            if (r.first) {
                 temp->text = r.second;
+            }
         }
         else if (args.key == GDK_KEY_Escape) {
             core.r->revert();
