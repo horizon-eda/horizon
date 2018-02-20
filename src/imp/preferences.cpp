82efb31f795519850b0dac059400034131334c2f
Lukas K <lu@0x83.eu>
2018-02-20 02:21:32 +0100

    preferences: ignore unknown actions

--- src/imp/preferences.cpp
+++ src/imp/preferences.cpp
@@ -131,21 +131,23 @@ void KeySequencesPreferences::load_from_json(const json &j)
 {
     keys.clear();
     for (const auto &it : j) {
-        auto action = action_lut.lookup(it.at("action"));
-        auto tool = tool_lut.lookup(it.at("tool"));
-        auto k = std::make_pair(action, tool);
-        keys[k];
-        auto &j2 = it.at("keys");
-        for (auto it2 = j2.cbegin(); it2 != j2.cend(); ++it2) {
-            auto av = static_cast<ActionCatalogItem::Availability>(std::stoi(it2.key()));
-            keys[k][av];
-            for (const auto &it3 : it2.value()) {
-                keys[k][av].emplace_back();
-                for (const auto &it4 : it3) {
-                    std::string keyname = it4.at("key");
-                    auto key = gdk_keyval_from_name(keyname.c_str());
-                    auto mod = static_cast<GdkModifierType>(it4.at("mod").get<int>());
-                    keys[k][av].back().emplace_back(key, mod);
+        auto action = action_lut.lookup(it.at("action"), ActionID::NONE);
+        if (action != ActionID::NONE) {
+            auto tool = tool_lut.lookup(it.at("tool"));
+            auto k = std::make_pair(action, tool);
+            keys[k];
+            auto &j2 = it.at("keys");
+            for (auto it2 = j2.cbegin(); it2 != j2.cend(); ++it2) {
+                auto av = static_cast<ActionCatalogItem::Availability>(std::stoi(it2.key()));
+                keys[k][av];
+                for (const auto &it3 : it2.value()) {
+                    keys[k][av].emplace_back();
+                    for (const auto &it4 : it3) {
+                        std::string keyname = it4.at("key");
+                        auto key = gdk_keyval_from_name(keyname.c_str());
+                        auto mod = static_cast<GdkModifierType>(it4.at("mod").get<int>());
+                        keys[k][av].back().emplace_back(key, mod);
+                    }
                 }
             }
         }
