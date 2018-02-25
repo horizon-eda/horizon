0ec5457883bf7bf25868ba0b4f0d21e3572d5573
Lukas K <lu@0x83.eu>
2018-02-25 18:52:33 +0100

    selection filter improvements

--- src/imp/selection_filter_dialog.cpp
+++ src/imp/selection_filter_dialog.cpp
@@ -1,19 +1,13 @@
 #include "selection_filter_dialog.hpp"
+#include "util/gtk_util.hpp"
 
 namespace horizon {
 
-static void header_fun(Gtk::ListBoxRow *row, Gtk::ListBoxRow *before)
-{
-    if (before && !row->get_header()) {
-        auto ret = Gtk::manage(new Gtk::Separator);
-        row->set_header(*ret);
-    }
-}
 
 SelectionFilterDialog::SelectionFilterDialog(Gtk::Window *parent, SelectionFilter *sf, Core *c)
     : Gtk::Window(), selection_filter(sf), core(c)
 {
-    set_default_size(200, 300);
+    set_default_size(220, 300);
     set_type_hint(Gdk::WINDOW_TYPE_HINT_DIALOG);
     set_transient_for(*parent);
     auto hb = Gtk::manage(new Gtk::HeaderBar());
@@ -23,7 +17,8 @@ SelectionFilterDialog::SelectionFilterDialog(Gtk::Window *parent, SelectionFilte
     set_title("Selection filter");
 
     auto reset_button = Gtk::manage(new Gtk::Button());
-    reset_button->set_image_from_icon_name("edit-select-all-symbolic", Gtk::ICON_SIZE_BUTTON);
+    reset_button->set_image_from_icon_name("object-select-symbolic", Gtk::ICON_SIZE_BUTTON);
+    reset_button->set_tooltip_text("Select all");
     reset_button->show_all();
     reset_button->signal_clicked().connect([this] {
         for (auto cb : checkbuttons) {
@@ -33,15 +28,9 @@ SelectionFilterDialog::SelectionFilterDialog(Gtk::Window *parent, SelectionFilte
     hb->pack_start(*reset_button);
     reset_button->show();
 
-    /*auto cssp = Gtk::CssProvider::create();
-    cssp->load_from_data(".imp-tiny-button {min-width:0px; min-height:0px;
-    padding:0px;}");
-    Gtk::StyleContext::add_provider_for_screen(Gdk::Screen::get_default(), cssp,
-    700);*/
-
     listbox = Gtk::manage(new Gtk::ListBox());
     listbox->set_selection_mode(Gtk::SELECTION_NONE);
-    listbox->set_header_func(sigc::ptr_fun(&header_fun));
+    listbox->set_header_func(sigc::ptr_fun(&header_func_separator));
     for (const auto &it : object_descriptions) {
         auto ot = it.first;
         if (ot == ObjectType::POLYGON)
@@ -49,26 +38,25 @@ SelectionFilterDialog::SelectionFilterDialog(Gtk::Window *parent, SelectionFilte
         if (core->has_object_type(ot)) {
             auto cb = Gtk::manage(new Gtk::CheckButton(it.second.name_pl));
             cb->set_active(true);
+            cb->property_margin() = 3;
+            cb->set_tooltip_text("Double click to select only this");
             cb->signal_toggled().connect([this, ot, cb] { selection_filter->object_filter[ot] = cb->get_active(); });
+            cb->signal_button_press_event().connect(
+                    [this, cb](GdkEventButton *ev) {
+                        if (ev->type == GDK_2BUTTON_PRESS) {
+                            for (auto cb_other : checkbuttons) {
+                                cb_other->set_active(cb == cb_other);
+                            }
+                        }
+                        else if (ev->type == GDK_BUTTON_PRESS) {
+                            cb->set_active(!cb->get_active());
+                        }
+                        return true;
+                    },
+                    false);
             checkbuttons.push_back(cb);
-            auto bbox = Gtk::manage(new Gtk::Box(Gtk::ORIENTATION_HORIZONTAL, 2));
 
-            auto only_button = Gtk::manage(new Gtk::Button());
-            only_button->set_margin_start(5);
-            // only_button->set_margin_top(2);
-            // only_button->set_margin_bottom(1);
-            only_button->set_image_from_icon_name("pan-end-symbolic", Gtk::ICON_SIZE_BUTTON);
-            only_button->set_relief(Gtk::RELIEF_NONE);
-            only_button->signal_clicked().connect([this, ot, cb] {
-                for (auto cb_other : checkbuttons) {
-                    cb_other->set_active(cb_other == cb);
-                }
-            });
-
-            bbox->pack_start(*only_button, false, false, 0);
-            bbox->pack_start(*cb, true, true, 0);
-
-            listbox->append(*bbox);
+            listbox->append(*cb);
         }
     }
 
