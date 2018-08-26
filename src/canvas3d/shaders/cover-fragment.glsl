bacb1369fd841ea104cfbf1dfcd467a30e8c4883
Lukas K <lu@0x83.eu>
2018-08-26 21:00:48 +0200

    add some ambient lighting

--- src/canvas3d/shaders/cover-fragment.glsl
+++ src/canvas3d/shaders/cover-fragment.glsl
@@ -6,5 +6,5 @@ in vec3 normal_to_fragment;
 uniform vec3 cam_normal;
 
 void main() {
-  outputColor = color_to_fragment*abs(dot(cam_normal, normal_to_fragment));
+  outputColor = color_to_fragment*(abs(dot(cam_normal, normal_to_fragment))+.1);
 }
