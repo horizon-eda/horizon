b3fd91a52eea2fcbe63d6c917d13c3aef7a3e768
Lukas K <lu@0x83.eu>
2019-02-10 01:10:31 +0100

    don't crash on missing pool.json

--- src/pool/pool_manager.cpp
+++ src/pool/pool_manager.cpp
@@ -63,7 +63,7 @@ PoolManager::PoolManager()
             for (auto it = o.cbegin(); it != o.cend(); ++it) {
                 std::string pool_base_path = it.key();
                 bool enabled = it.value();
-                if (Glib::file_test(pool_base_path, Glib::FILE_TEST_IS_DIR)) {
+                if (Glib::file_test(Glib::build_filename(pool_base_path, "pool.json"), Glib::FILE_TEST_IS_REGULAR)) {
                     pools.emplace(std::piecewise_construct, std::forward_as_tuple(pool_base_path),
                                   std::forward_as_tuple(pool_base_path));
                     set_pool_enabled_no_write(pool_base_path, enabled);
@@ -77,7 +77,7 @@ PoolManager::PoolManager()
             auto o = j.at("pools");
             for (auto it = o.cbegin(); it != o.cend(); ++it) {
                 std::string pool_base_path = Glib::path_get_dirname(it.value());
-                if (Glib::file_test(pool_base_path, Glib::FILE_TEST_IS_DIR)) {
+                if (Glib::file_test(Glib::build_filename(pool_base_path, "pool.json"), Glib::FILE_TEST_IS_REGULAR)) {
                     pools.emplace(std::piecewise_construct, std::forward_as_tuple(pool_base_path),
                                   std::forward_as_tuple(pool_base_path));
                 }
