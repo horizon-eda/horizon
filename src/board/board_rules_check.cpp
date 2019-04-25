f7e9e0f0adb9def19b5a203a8e1876d181b55f7d
Lukas K <lu@0x83.eu>
2019-04-25 20:05:48 +0200

    fix non-copper clearance if there's no npth hole

--- src/board/board_rules_check.cpp
+++ src/board/board_rules_check.cpp
@@ -290,51 +290,51 @@ RulesCheckResult BoardRules::check_clearance_copper_non_copper(const Board *brd,
     npth_key.layer = 10000;
     npth_key.net = UUID();
     npth_key.type = PatchType::HOLE_NPTH;
-    if (patches.count(npth_key) == 0) {
-        return r;
-    }
-    auto &patch_npth = patches.at(npth_key);
+    if (patches.count(npth_key)) {
+        auto &patch_npth = patches.at(npth_key);
 
-    for (const auto &it : patches) {
-        if (brd->get_layers().count(it.first.layer)
-            && brd->get_layers().at(it.first.layer).copper) { // need to check copper layer
-            Net *net = it.first.net ? &brd->block->nets.at(it.first.net) : nullptr;
+        for (const auto &it : patches) {
+            if (brd->get_layers().count(it.first.layer)
+                && brd->get_layers().at(it.first.layer).copper) { // need to check copper layer
+                Net *net = it.first.net ? &brd->block->nets.at(it.first.net) : nullptr;
 
-            auto clearance =
-                    get_clearance_copper_other(net, it.first.layer)->get_clearance(it.first.type, PatchType::HOLE_NPTH);
+                auto clearance = get_clearance_copper_other(net, it.first.layer)
+                                         ->get_clearance(it.first.type, PatchType::HOLE_NPTH);
 
-            // expand npth patch by clearance
-            ClipperLib::ClipperOffset ofs;
-            ofs.ArcTolerance = 10e3;
-            ofs.AddPaths(patch_npth, ClipperLib::jtRound, ClipperLib::etClosedPolygon);
-            ClipperLib::Paths paths_ofs;
-            ofs.Execute(paths_ofs, clearance);
+                // expand npth patch by clearance
+                ClipperLib::ClipperOffset ofs;
+                ofs.ArcTolerance = 10e3;
+                ofs.AddPaths(patch_npth, ClipperLib::jtRound, ClipperLib::etClosedPolygon);
+                ClipperLib::Paths paths_ofs;
+                ofs.Execute(paths_ofs, clearance);
 
-            // intersect expanded and this patch
-            ClipperLib::Clipper clipper;
-            clipper.AddPaths(paths_ofs, ClipperLib::ptClip, true);
-            clipper.AddPaths(it.second, ClipperLib::ptSubject, true);
-            ClipperLib::Paths errors;
-            clipper.Execute(ClipperLib::ctIntersection, errors, ClipperLib::pftNonZero, ClipperLib::pftNonZero);
+                // intersect expanded and this patch
+                ClipperLib::Clipper clipper;
+                clipper.AddPaths(paths_ofs, ClipperLib::ptClip, true);
+                clipper.AddPaths(it.second, ClipperLib::ptSubject, true);
+                ClipperLib::Paths errors;
+                clipper.Execute(ClipperLib::ctIntersection, errors, ClipperLib::pftNonZero, ClipperLib::pftNonZero);
 
-            // no intersection: no clearance violation
-            if (errors.size() > 0) {
-                for (const auto &ite : errors) {
-                    r.errors.emplace_back(RulesCheckErrorLevel::FAIL);
-                    auto &e = r.errors.back();
-                    e.has_location = true;
-                    Accumulator<Coordi> acc;
-                    for (const auto &ite2 : ite) {
-                        acc.accumulate({ite2.X, ite2.Y});
+                // no intersection: no clearance violation
+                if (errors.size() > 0) {
+                    for (const auto &ite : errors) {
+                        r.errors.emplace_back(RulesCheckErrorLevel::FAIL);
+                        auto &e = r.errors.back();
+                        e.has_location = true;
+                        Accumulator<Coordi> acc;
+                        for (const auto &ite2 : ite) {
+                            acc.accumulate({ite2.X, ite2.Y});
+                        }
+                        e.location = acc.get();
+                        e.comment =
+                                patch_type_names.at(it.first.type) + "(" + (net ? net->name : "") + ") near NPTH hole";
+                        e.error_polygons = {ite};
                     }
-                    e.location = acc.get();
-                    e.comment = patch_type_names.at(it.first.type) + "(" + (net ? net->name : "") + ") near NPTH hole";
-                    e.error_polygons = {ite};
                 }
             }
         }
+        r.update();
     }
-    r.update();
 
     // other cu
     std::set<int> layers;
