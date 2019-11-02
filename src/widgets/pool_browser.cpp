e1d56a3d48ff321ed3478d2c49832f2b893666a8
Lukas K <lu@0x83.eu>
2019-11-02 18:18:43 +0100

    fix build for gtkmm<3.24

--- src/widgets/pool_browser.cpp
+++ src/widgets/pool_browser.cpp
@@ -141,7 +141,8 @@ void PoolBrowser::construct(Gtk::Widget *search_box)
         overlay->add(*sc);
         overlay->show_all();
         busy_box = Gtk::manage(new Gtk::Box(Gtk::ORIENTATION_VERTICAL, 4));
-        auto img = Gtk::manage(new Gtk::Image("hourglass-symbolic", Gtk::ICON_SIZE_DIALOG));
+        auto img = Gtk::manage(new Gtk::Image);
+        img->set_from_icon_name("hourglass-symbolic", Gtk::ICON_SIZE_DIALOG);
         busy_box->pack_start(*img, true, true, 0);
         auto la = Gtk::manage(new Gtk::Label("Database is busy"));
         busy_box->pack_start(*la, true, true, 0);
