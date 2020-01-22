8e841128188e7c3b51e625ff2140184da8532cfd
Lukas K <lu@0x83.eu>
2020-01-23 00:04:01 +0100

    tool add part: autoconnect symbol

--- src/core/tool_add_part.cpp
+++ src/core/tool_add_part.cpp
@@ -97,7 +97,10 @@ ToolResponse ToolAddPart::update(const ToolArgs &args)
     }
     else if (args.type == ToolEventType::CLICK) {
         if (args.button == 1) {
-            core.c->get_schematic()->place_bipole_on_line(core.c->get_sheet(), sym_current);
+            core.c->get_schematic()->autoconnect_symbol(core.c->get_sheet(), sym_current);
+            if (sym_current->component->connections.size() == 0) {
+                core.c->get_schematic()->place_bipole_on_line(core.c->get_sheet(), sym_current);
+            }
             if (current_gate + 1 == gates.size()) { // last gate
                 current_gate = 0;
                 // add next component
