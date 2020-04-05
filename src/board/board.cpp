407d6aca28a83f51063db6dcfa5df1f1e90bdd97
Lukas K <lu@0x83.eu>
2020-04-05 13:24:54 +0200

    correctly handle outline layer for flipped packages

--- src/board/board.cpp
+++ src/board/board.cpp
@@ -447,7 +447,9 @@ void Board::update_pdf_export_settings(PDFExportSettings &settings)
 
 void Board::flip_package_layer(int &layer) const
 {
-    if (layer < 0 && layer > -100) {
+    if (layer == BoardLayers::L_OUTLINE)
+        return;
+    else if (layer < 0 && layer > -100) {
         if (n_inner_layers == 0)
             return;
         if (-layer > (int)n_inner_layers)
