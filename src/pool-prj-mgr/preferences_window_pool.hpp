5ea364e32e4c6b5e8b32c5362623801900931632
Lukas K <lu@0x83.eu>
2020-05-13 12:29:12 +0200

    revamp pool preferences

--- src/pool-prj-mgr/preferences_window_pool.hpp
+++ src/pool-prj-mgr/preferences_window_pool.hpp
@@ -2,7 +2,7 @@
 #include <gtkmm.h>
 
 namespace horizon {
-class PoolPreferencesEditor : public Gtk::Box {
+class PoolPreferencesEditor : public Gtk::ScrolledWindow {
 public:
     PoolPreferencesEditor(BaseObjectType *cobject, const Glib::RefPtr<Gtk::Builder> &x);
     static PoolPreferencesEditor *create();
@@ -12,7 +12,6 @@ public:
 private:
     class PoolManager &mgr;
     Gtk::ListBox *listbox = nullptr;
-    Gtk::Button *button_add_pool = nullptr;
     Glib::RefPtr<Gtk::SizeGroup> size_group;
     void update();
 };
