6c8fd8ef8b769e7f61fc83a59caefddebdf15fd9
Lukas K <lu@0x83.eu>
2020-06-28 01:38:23 +0200

    make zero-width selectables smaller

--- src/canvas/shaders/selectable-geometry.glsl
+++ src/canvas/shaders/selectable-geometry.glsl
@@ -35,26 +35,37 @@ void main() {
 	float angle = -angle_to_geom[0];
 	
 	const float min_sz = 10;
-	bool no_bb = (bb_tr.x==bb_bl.x)&&(bb_tr.y == bb_bl.y);
+	vec2 size = abs(bb_tr-bb_bl);
+	bool no_bb = (size.x == 0) && (size.y == 0);
 	float origin_size = 10/scale;
 	if(no_bb)
 		origin_size = 7/scale;
 	
 	
-	if(abs(bb_tr.x - bb_bl.x) < min_sz/scale) {
-		bb_tr.x += .5*min_sz/scale;
-		bb_bl.x -= .5*min_sz/scale;
+	if((size.x > 0) && (size.x < min_sz/scale)) {
+		bb_tr.x = +.5*min_sz/scale;
+		bb_bl.x = -.5*min_sz/scale;
 	}
-	if(abs(bb_tr.y - bb_bl.y) < min_sz/scale) {
-		bb_tr.y += .5*min_sz/scale;
-		bb_bl.y -= .5*min_sz/scale;
+	if((size.y > 0) && (size.y < min_sz/scale)) {
+		bb_tr.y = +.5*min_sz/scale;
+		bb_bl.y = -.5*min_sz/scale;
 	}
 	
-	float border_width = 5;
-	bb_tr.x += border_width/scale;
-	bb_bl.x -= border_width/scale;
-	bb_tr.y += border_width/scale;
-	bb_bl.y -= border_width/scale;
+	vec2 border_width = vec2(5,5);
+	if(size.x == 0) {
+		border_width.x = 1;
+		border_width.y = 0;
+	}
+	if(size.y == 0) {
+		border_width.y = 1;
+		border_width.x = 0;
+	}
+		
+		
+	bb_tr.x += border_width.x/scale;
+	bb_bl.x -= border_width.x/scale;
+	bb_tr.y += border_width.y/scale;
+	bb_bl.y -= border_width.y/scale;
 	
 	uint flags = flags_to_geom[0];
 	if(flags == uint(0)) {
@@ -74,8 +85,31 @@ void main() {
 	}
 	dot_to_fragment = vec2(0,0);
 	
-	vec3 c_save = color_to_fragment;
 	
+	if(!no_bb) {
+		float wp = abs((bb_tr.x-bb_bl.x)*scale);
+		float hp = abs((bb_tr.y-bb_bl.y)*scale);
+		size_to_fragment = vec2(wp, hp);
+		
+		gl_Position = t(box_center+(rotate2d(angle)*vec2(bb_bl.x, bb_tr.y)));
+		dot_to_fragment = vec2(0,0);
+		EmitVertex();
+		
+		gl_Position = t(box_center+(rotate2d(angle)*vec2(bb_tr.x, bb_tr.y)));
+		dot_to_fragment = vec2(wp,0);
+		EmitVertex();
+		
+		gl_Position = t(box_center+(rotate2d(angle)*vec2(bb_bl.x, bb_bl.y)));
+		dot_to_fragment = vec2(0,hp);
+		EmitVertex();
+		
+		gl_Position = t(box_center+(rotate2d(angle)*vec2(bb_tr.x, bb_bl.y)));
+		dot_to_fragment = vec2(wp,hp);
+		EmitVertex();
+		
+		EndPrimitive();
+	}
+
 	float os = origin_size;
 	size_to_fragment = vec2(-1, -1);
 	
@@ -108,30 +142,4 @@ void main() {
 	EmitVertex();
 	
 	EndPrimitive();
-	
-	color_to_fragment = c_save;
-	
-	if(!no_bb) {
-		float wp = abs((bb_tr.x-bb_bl.x)*scale);
-		float hp = abs((bb_tr.y-bb_bl.y)*scale);
-		size_to_fragment = vec2(wp, hp);
-		
-		gl_Position = t(box_center+(rotate2d(angle)*vec2(bb_bl.x, bb_tr.y)));
-		dot_to_fragment = vec2(0,0);
-		EmitVertex();
-		
-		gl_Position = t(box_center+(rotate2d(angle)*vec2(bb_tr.x, bb_tr.y)));
-		dot_to_fragment = vec2(wp,0);
-		EmitVertex();
-		
-		gl_Position = t(box_center+(rotate2d(angle)*vec2(bb_bl.x, bb_bl.y)));
-		dot_to_fragment = vec2(0,hp);
-		EmitVertex();
-		
-		gl_Position = t(box_center+(rotate2d(angle)*vec2(bb_tr.x, bb_bl.y)));
-		dot_to_fragment = vec2(wp,hp);
-		EmitVertex();
-		
-		EndPrimitive();
-	}
 }
