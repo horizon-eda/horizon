ac5dda23d3a249692eb575777da53b45987dd465
Lukas K <lu@0x83.eu>
2020-09-15 12:11:13 +0200

    symbol checks: check refdes/value position rel. to box

--- src/symbol/symbol_rules_check.cpp
+++ src/symbol/symbol_rules_check.cpp
@@ -16,60 +16,101 @@ RulesCheckResult SymbolRules::check_symbol(const Symbol &sym) const
         x.comment = "Package has no name";
         x.has_location = false;
     }
-
     {
         const Text *txt_refdes = nullptr;
         const Text *txt_value = nullptr;
         {
-            int refdes_count = 0;
-            int value_count = 0;
-            for (const auto &[uu, txt] : sym.texts) {
-                if (txt.text == "$REFDES") {
-                    refdes_count++;
-                    txt_refdes = &txt;
+            {
+                int refdes_count = 0;
+                int value_count = 0;
+                for (const auto &[uu, txt] : sym.texts) {
+                    if (txt.text == "$REFDES") {
+                        refdes_count++;
+                        txt_refdes = &txt;
+                    }
+                    else if (txt.text == "$VALUE") {
+                        value_count++;
+                        txt_value = &txt;
+                    }
                 }
-                else if (txt.text == "$VALUE") {
-                    value_count++;
-                    txt_value = &txt;
-                }
-            }
-            if (refdes_count != 1) {
-                r.errors.emplace_back(RulesCheckErrorLevel::FAIL);
-                auto &x = r.errors.back();
-                x.comment = "Symbol has " + std::to_string(refdes_count) + " reference designators, need 1";
-                x.has_location = false;
-            }
-            if (value_count != 1) {
-                r.errors.emplace_back(RulesCheckErrorLevel::FAIL);
-                auto &x = r.errors.back();
-                x.comment = "Symbol has " + std::to_string(refdes_count) + " value, need 1";
-                x.has_location = false;
-            }
-        }
-        auto check_text = [&r](const Text *txt, const std::string &name) {
-            if (txt) {
-                if (txt->width != 0 || txt->size != 1.5_mm) {
+                if (refdes_count != 1) {
                     r.errors.emplace_back(RulesCheckErrorLevel::FAIL);
                     auto &x = r.errors.back();
-                    x.comment = name + " text must have 0 line width and 1.5mm size";
+                    x.comment = "Symbol has " + std::to_string(refdes_count) + " reference designators, need 1";
+                    x.has_location = false;
+                }
+                if (value_count != 1) {
+                    r.errors.emplace_back(RulesCheckErrorLevel::FAIL);
+                    auto &x = r.errors.back();
+                    x.comment = "Symbol has " + std::to_string(refdes_count) + " value, need 1";
                     x.has_location = false;
                 }
             }
-        };
-        check_text(txt_refdes, "Refdes");
-        check_text(txt_value, "Value");
-        if (txt_refdes && txt_value) {
-            if (txt_refdes->placement.shift.x != txt_value->placement.shift.x) {
-                r.errors.emplace_back(RulesCheckErrorLevel::WARN);
-                auto &x = r.errors.back();
-                x.comment = "Refdes and value text should be at same X position";
-                x.has_location = false;
+            auto check_text = [&r](const Text *txt, const std::string &name) {
+                if (txt) {
+                    if (txt->width != 0 || txt->size != 1.5_mm) {
+                        r.errors.emplace_back(RulesCheckErrorLevel::FAIL);
+                        auto &x = r.errors.back();
+                        x.comment = name + " text must have 0 line width and 1.5mm size";
+                        x.has_location = false;
+                    }
+                }
+            };
+            check_text(txt_refdes, "Refdes");
+            check_text(txt_value, "Value");
+            if (txt_refdes && txt_value) {
+                if (txt_refdes->placement.shift.x != txt_value->placement.shift.x) {
+                    r.errors.emplace_back(RulesCheckErrorLevel::WARN);
+                    auto &x = r.errors.back();
+                    x.comment = "Refdes and value text should be at same X position";
+                    x.has_location = false;
+                }
+                if (txt_refdes->placement.shift.y != -txt_value->placement.shift.y) {
+                    r.errors.emplace_back(RulesCheckErrorLevel::WARN);
+                    auto &x = r.errors.back();
+                    x.comment = "Refdes and value text should be at mirrored Y position";
+                    x.has_location = false;
+                }
+                if (txt_value->placement.shift.y > txt_refdes->placement.shift.y) {
+                    r.errors.emplace_back(RulesCheckErrorLevel::WARN);
+                    auto &x = r.errors.back();
+                    x.comment = "Refdes should be above value";
+                    x.has_location = false;
+                }
             }
-            if (txt_refdes->placement.shift.y != -txt_value->placement.shift.y) {
-                r.errors.emplace_back(RulesCheckErrorLevel::WARN);
+        }
+        {
+            Coordi bb;
+            for (const auto &[uu, junction] : sym.junctions) {
+                const auto &p = junction.position;
+                bb = Coordi::max(bb, {std::abs(p.x), std::abs(p.y)});
+            }
+            auto find_junction = [&sym](const Coordi &p) {
+                return std::any_of(sym.junctions.begin(), sym.junctions.end(),
+                                   [&p](const auto &x) { return x.second.position == p; });
+            };
+            const bool is_box = find_junction(bb * Coordi(1, 1)) && find_junction(bb * Coordi(1, -1))
+                                && find_junction(bb * Coordi(-1, 1)) && find_junction(bb * Coordi(-1, -1));
+            if (is_box) {
+                r.errors.emplace_back(RulesCheckErrorLevel::PASS);
                 auto &x = r.errors.back();
-                x.comment = "Refdes and value text should be at mirrored Y position";
+                x.comment = "Is box symbol";
                 x.has_location = false;
+                const int64_t txt_spacing = 1.25_mm;
+                if (txt_refdes && txt_refdes->placement.shift.y != bb.y + txt_spacing) {
+                    r.errors.emplace_back(RulesCheckErrorLevel::WARN);
+                    auto &x = r.errors.back();
+                    x.comment = "Refdes should be 1.25mm above symbol box";
+                    x.has_location = true;
+                    x.location = txt_refdes->placement.shift;
+                }
+                if (txt_value && txt_value->placement.shift.y != -bb.y - txt_spacing) {
+                    r.errors.emplace_back(RulesCheckErrorLevel::WARN);
+                    auto &x = r.errors.back();
+                    x.comment = "Value should be 1.25mm below symbol box";
+                    x.has_location = true;
+                    x.location = txt_value->placement.shift;
+                }
             }
         }
     }
