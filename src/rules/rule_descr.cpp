f531970c6d3fe6d7be5bf84077a4be18a52ce773
Lukas K <lu@0x83.eu>
2020-09-23 21:50:11 +0200

    make rule description easier to read
    by using enum rather than bool

--- src/rules/rule_descr.cpp
+++ src/rules/rule_descr.cpp
@@ -1,23 +1,44 @@
 #include "rule_descr.hpp"
 
 namespace horizon {
-const std::map<RuleID, RuleDescription> rule_descriptions = {
-        {RuleID::HOLE_SIZE, {"Hole size", true, true}},
-        {RuleID::TRACK_WIDTH, {"Track width", true, true, true}},
-        {RuleID::CLEARANCE_COPPER, {"Copper clearance", true, true, true}},
-        {RuleID::CLEARANCE_SILKSCREEN_EXPOSED_COPPER, {"Clearance\nSilkscreen - Exposed copper", false, false}},
-        {RuleID::PARAMETERS, {"Parameters", false, false}},
-        {RuleID::SINGLE_PIN_NET, {"Single pin nets", false, true}},
-        {RuleID::VIA, {"Vias", true, false, true}},
-        {RuleID::CLEARANCE_COPPER_OTHER, {"Clearance Copper - Other", true, true, true}},
-        {RuleID::PLANE, {"Planes", true, false, true}},
-        {RuleID::DIFFPAIR, {"Diffpair", true, false}},
-        {RuleID::PACKAGE_CHECKS, {"Package checks", false, true}},
-        {RuleID::PREFLIGHT_CHECKS, {"Preflight checks", false, true}},
-        {RuleID::CLEARANCE_COPPER_KEEPOUT, {"Clearance Copper - Keepout", true, true, true}},
-        {RuleID::LAYER_PAIR, {"Layer pairs", true, false, false}},
-        {RuleID::CLEARANCE_SAME_NET, {"Same net clearance", true, true, false}},
-        {RuleID::SYMBOL_CHECKS, {"Symbol checks", false, true}},
-        {RuleID::CLEARANCE_PACKAGE, {"Package clearance", false, true}},
+
+using RD = RuleDescription;
+
+const std::map<RuleID, RD> rule_descriptions = {
+        {RuleID::HOLE_SIZE, {"Hole size", RD::IS_MULTI | RD::CAN_CHECK}},
+
+        {RuleID::TRACK_WIDTH, {"Track width", RD::IS_MULTI | RD::CAN_CHECK | RD::CAN_APPLY | RD::NEEDS_MATCH_ALL}},
+
+        {RuleID::CLEARANCE_COPPER, {"Copper clearance", RD::IS_MULTI | RD::CAN_CHECK | RD::NEEDS_MATCH_ALL}},
+
+        {RuleID::CLEARANCE_SILKSCREEN_EXPOSED_COPPER, {"Clearance\nSilkscreen - Exposed copper", 0}},
+
+        {RuleID::PARAMETERS, {"Parameters", RD::CAN_APPLY}},
+
+        {RuleID::SINGLE_PIN_NET, {"Single pin nets", RD::CAN_CHECK}},
+
+        {RuleID::VIA, {"Vias", RD::IS_MULTI | RD::CAN_APPLY | RD::NEEDS_MATCH_ALL}},
+
+        {RuleID::CLEARANCE_COPPER_OTHER,
+         {"Clearance Copper - Other", RD::IS_MULTI | RD::CAN_CHECK | RD::NEEDS_MATCH_ALL}},
+
+        {RuleID::PLANE, {"Planes", RD::IS_MULTI | RD::CAN_APPLY | RD::NEEDS_MATCH_ALL}},
+
+        {RuleID::DIFFPAIR, {"Diffpair", RD::IS_MULTI}},
+
+        {RuleID::PACKAGE_CHECKS, {"Package checks", RD::CAN_CHECK}},
+
+        {RuleID::PREFLIGHT_CHECKS, {"Preflight checks", RD::CAN_CHECK}},
+
+        {RuleID::CLEARANCE_COPPER_KEEPOUT,
+         {"Clearance Copper - Keepout", RD::IS_MULTI | RD::CAN_CHECK | RD::NEEDS_MATCH_ALL}},
+
+        {RuleID::LAYER_PAIR, {"Layer pairs", RD::IS_MULTI}},
+
+        {RuleID::CLEARANCE_SAME_NET, {"Same net clearance", RD::IS_MULTI | RD::CAN_CHECK}},
+
+        {RuleID::SYMBOL_CHECKS, {"Symbol checks", RD::CAN_CHECK}},
+
+        {RuleID::CLEARANCE_PACKAGE, {"Package clearance", RD::CAN_CHECK}},
 };
-}
+} // namespace horizon
--- src/rules/rule_descr.hpp
+++ src/rules/rule_descr.hpp
@@ -5,14 +5,23 @@
 namespace horizon {
 class RuleDescription {
 public:
-    RuleDescription(const std::string &n, bool m, bool c, bool a = false)
-        : name(n), multi(m), can_check(c), needs_match_all(a)
+    enum Flag {
+        IS_MULTI = (1 << 0),
+        CAN_CHECK = (1 << 1),
+        CAN_APPLY = (1 << 2),
+        NEEDS_MATCH_ALL = (1 << 3),
+    };
+
+    RuleDescription(const std::string &n, unsigned int flags)
+        : name(n), multi(flags & IS_MULTI), can_check(flags & CAN_CHECK), can_apply(flags & CAN_APPLY),
+          needs_match_all(flags & NEEDS_MATCH_ALL)
     {
     }
 
     std::string name;
     bool multi;
     bool can_check;
+    bool can_apply;
     bool needs_match_all;
 };
 
