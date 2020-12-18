501027f6d5fe1c6071fe21bb33422808a04dcc18
Lukas K <lu@0x83.eu>
2020-12-18 13:14:18 +0100

    KeepSlopeInfo: improve constness

--- src/util/keep_slope_util.cpp
+++ src/util/keep_slope_util.cpp
@@ -3,12 +3,13 @@
 namespace horizon {
 std::pair<Coordi, Coordi> KeepSlopeInfo::get_pos(const Coordd &shift) const
 {
-    Coordd vfrom = pos_from_orig - pos_from2;
-    Coordd vto = pos_to_orig - pos_to2;
-    Coordd vtr = pos_to_orig - pos_from_orig;
-    Coordd vtrn(vtr.y, -vtr.x);
+    const Coordd vfrom = pos_from_orig - pos_from2;
+    const Coordd vto = pos_to_orig - pos_to2;
+    const Coordd vtr = pos_to_orig - pos_from_orig;
+    const Coordd vtrn(vtr.y, -vtr.x);
 
-    Coordd vshift2 = (vtrn * (vtrn.dot(shift))) / vtrn.mag_sq();
+    // shift projected onto vector perpendicular to track
+    const Coordd vshift2 = (vtrn * (vtrn.dot(shift))) / vtrn.mag_sq();
 
     Coordd shift_from = (vfrom * vshift2.mag_sq()) / (vfrom.dot(vshift2));
     Coordd shift_to = (vto * vshift2.mag_sq()) / (vto.dot(vshift2));
