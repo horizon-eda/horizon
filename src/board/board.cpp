245b47ec48ae23a060be5aa72e66f10b2e296221
Lukas K <lu@0x83.eu>
2020-12-31 14:53:07 +0100

    bump board app version
    for pnp export customisation: 3ecc1bfe11efa193392d353a12f95c4f781d3ac8

--- src/board/board.cpp
+++ src/board/board.cpp
@@ -33,7 +33,7 @@ BoardColors::BoardColors() : solder_mask({0, .5, 0}), substrate({.2, .15, 0})
 {
 }
 
-static const unsigned int app_version = 1;
+static const unsigned int app_version = 2;
 
 unsigned int Board::get_app_version()
 {
