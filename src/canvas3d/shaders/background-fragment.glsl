67e0c4b642dfd286aa88de03bf652c553c13af45
Lukas K <lu@0x83.eu>
2021-01-05 14:30:49 +0100

    3d preview: use gamma-correct background gradient

--- src/canvas3d/shaders/background-fragment.glsl
+++ src/canvas3d/shaders/background-fragment.glsl
@@ -3,5 +3,5 @@ out vec4 outputColor;
 in vec3 color_to_fragment;
 
 void main() {
-	outputColor = vec4(color_to_fragment,1);
+	outputColor = vec4(pow(color_to_fragment, vec3(1/2.2)),1);
 }
--- src/canvas3d/shaders/background-vertex.glsl
+++ src/canvas3d/shaders/background-vertex.glsl
@@ -7,10 +7,10 @@ uniform vec3 color_bottom;
 
 void main() {
 	if(gl_VertexID < 2) {
-		color_to_fragment = color_top;
+		color_to_fragment = pow(color_top, vec3(2.2));
 	}
 	else {
-		color_to_fragment = color_bottom;
+		color_to_fragment = pow(color_bottom, vec3(2.2));
 	}
 	gl_Position = vec4(position, -1, 1);
 }
