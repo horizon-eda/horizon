30975bd3055c2b986b14ab8fa426aa03057f1537
Lukas K <lu@0x83.eu>
2021-02-03 00:42:42 +0100

    tool add part/map symbol: store symbol choice

--- src/core/tools/tool_helper_map_symbol.cpp
+++ src/core/tools/tool_helper_map_symbol.cpp
@@ -5,6 +5,8 @@
 #include "imp/imp_interface.hpp"
 #include "util/sqlite.hpp"
 #include "pool/ipool.hpp"
+#include "core/tool_id.hpp"
+#include "nlohmann/json.hpp"
 
 namespace horizon {
 const Symbol *ToolHelperMapSymbol::get_symbol_for_unit(const UUID &unit_uu, bool *auto_selected)
@@ -36,7 +38,7 @@ const Symbol *ToolHelperMapSymbol::get_symbol_for_unit(const UUID &unit_uu, bool
     else if (n == 0) {
         return nullptr;
     }
-    selected_symbols[unit_uu] = selected_symbol;
+    settings.selected_symbols[unit_uu] = selected_symbol;
 
     return doc.c->get_pool().get_symbol(selected_symbol);
 }
@@ -45,8 +47,8 @@ const Symbol *ToolHelperMapSymbol::get_symbol_for_unit(const UUID &unit_uu, bool
 SchematicSymbol *ToolHelperMapSymbol::map_symbol(Component *comp, const Gate *gate)
 {
     const Symbol *sym = nullptr;
-    if (selected_symbols.count(gate->unit->uuid))
-        sym = doc.c->get_pool().get_symbol(selected_symbols.at(gate->unit->uuid));
+    if (settings.selected_symbols.count(gate->unit->uuid))
+        sym = doc.c->get_pool().get_symbol(settings.selected_symbols.at(gate->unit->uuid));
     if (!sym)
         sym = get_symbol_for_unit(gate->unit->uuid);
     if (!sym)
@@ -73,9 +75,34 @@ void ToolHelperMapSymbol::change_symbol(SchematicSymbol *schsym)
             imp->tool_bar_flash("There's only one symbol for this unit");
         }
         else {
-            selected_symbols[gate->unit->uuid] = new_sym->uuid;
+            settings.selected_symbols[gate->unit->uuid] = new_sym->uuid;
             schsym->pool_symbol = new_sym;
             doc.c->get_sheet()->expand_symbol(schsym->uuid, *doc.c->get_schematic());
+        }
+    }
+}
+
+ToolID ToolHelperMapSymbol::get_tool_id_for_settings() const
+{
+    return ToolID::MAP_SYMBOL;
+}
+
+json ToolHelperMapSymbol::Settings::serialize() const
+{
+    json o = json::object();
+    for (const auto &[k, v] : selected_symbols) {
+        o[(std::string)k] = (std::string)v;
+    }
+    json j;
+    j["selected_symbols"] = selected_symbols;
+    return j;
+}
+
+void ToolHelperMapSymbol::Settings::load_from_json(const json &j)
+{
+    if (j.count("selected_symbols")) {
+        for (const auto &[k, v] : j.at("selected_symbols").items()) {
+            selected_symbols[UUID(k)] = UUID(v.get<std::string>());
         }
     }
 }
--- src/core/tools/tool_helper_map_symbol.hpp
+++ src/core/tools/tool_helper_map_symbol.hpp
@@ -6,12 +6,26 @@ class ToolHelperMapSymbol : public virtual ToolBase {
 public:
     using ToolBase::ToolBase;
 
+    class Settings : public ToolSettings {
+    public:
+        json serialize() const override;
+        void load_from_json(const json &j) override;
+        std::map<UUID, UUID> selected_symbols;
+    };
+
+    ToolSettings *get_settings() override
+    {
+        return &settings;
+    }
+
+    ToolID get_tool_id_for_settings() const override;
+
 protected:
     class SchematicSymbol *map_symbol(class Component *c, const class Gate *g);
     const class Symbol *get_symbol_for_unit(const UUID &unit_uu, bool *auto_selected = nullptr);
     void change_symbol(class SchematicSymbol *schsym);
 
 private:
-    std::map<UUID, UUID> selected_symbols;
+    Settings settings;
 };
 } // namespace horizon
