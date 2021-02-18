5d41bb284d055e9d0cbea2986b1abbb47484ec59
Lukas K <lu@0x83.eu>
2021-02-18 19:04:05 +0100

    part check: add reichelt and conrad to discouraged domains

--- src/checks/check_part.cpp
+++ src/checks/check_part.cpp
@@ -6,7 +6,8 @@
 namespace horizon {
 
 static const std::vector<std::string> discouraged_datasheet_domains = {
-        "rs-online.com", "digikey.com", "mouser.com", "farnell.com", "octopart.com", "google.com",
+        "rs-online.com", "digikey.com", "mouser.com", "farnell.com", "octopart.com",
+        "google.com",    "reichelt.de", "conrad.de",  "conrad.com",
 };
 
 
