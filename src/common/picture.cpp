86c32cb5932a92d51db7b38e9efd47e129d7eb75
Lukas K <lu@0x83.eu>
2021-03-17 21:48:18 +0100

    Picture: store on_top state

--- src/common/picture.cpp
+++ src/common/picture.cpp
@@ -9,7 +9,8 @@ Picture::Picture(const UUID &uu) : uuid(uu)
 }
 
 Picture::Picture(const UUID &uu, const json &j)
-    : uuid(uu), placement(j.at("placement")), px_size(j.at("px_size")), data_uuid(j.at("data").get<std::string>())
+    : uuid(uu), placement(j.at("placement")), on_top(j.value("on_top", false)), px_size(j.at("px_size")),
+      data_uuid(j.at("data").get<std::string>())
 {
 }
 
@@ -18,6 +19,7 @@ json Picture::serialize() const
     json j;
     j["uuid"] = (std::string)uuid;
     j["placement"] = placement.serialize();
+    j["on_top"] = on_top;
     j["px_size"] = px_size;
     j["data"] = (std::string)data_uuid;
     return j;
