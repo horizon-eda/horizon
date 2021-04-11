2ad75b1fd6847a35ccdb13a7a0975b983698078e
Lukas K <lu@0x83.eu>
2021-04-11 17:45:10 +0200

    pictures: save/load using libpng
    cairo surfaces use premultiplied alpha, but we need non-premultiplied
    alpha for rendering
    
    use libpng so load/save pictures using non-premultiplied alpha
    
    adapted from https://gitlab.freedesktop.org/cairo/cairo/-/blob/master/src/cairo-png.c

--- Makefile
+++ Makefile
@@ -789,7 +789,7 @@ LIBS_COMMON = sqlite3 libzip
 ifneq ($(OS),Windows_NT)
 	LIBS_COMMON += uuid
 endif
-LIBS_ALL = $(LIBS_COMMON) gtkmm-3.0 epoxy cairomm-pdf-1.0 librsvg-2.0 libzmq libgit2 libcurl glm
+LIBS_ALL = $(LIBS_COMMON) gtkmm-3.0 epoxy cairomm-pdf-1.0 librsvg-2.0 libzmq libgit2 libcurl glm libpng
 
 OPTIMIZE = -fdata-sections -ffunction-sections -O3
 DEBUGFLAGS = -g3
@@ -955,7 +955,7 @@ $(BUILDDIR)/gen/help_texts.hpp: scripts/make_help.py src/help_texts.txt
 
 $(BUILDDIR)/horizon-imp: $(OBJ_COMMON) $(OBJ_ROUTER) $(OBJ_OCE) $(OBJ_IMP)
 	$(ECHO) " $@"
-	$(QUIET)$(CXX) $^ $(LDFLAGS) $(LDFLAGS_GUI) $(LDFLAGS_OCE) $(shell $(PKG_CONFIG) --libs $(LIBS_COMMON) gtkmm-3.0 epoxy cairomm-pdf-1.0 librsvg-2.0 libzmq libcurl) -lpodofo -lTKHLR -lTKGeomBase -o $@
+	$(QUIET)$(CXX) $^ $(LDFLAGS) $(LDFLAGS_GUI) $(LDFLAGS_OCE) $(shell $(PKG_CONFIG) --libs $(LIBS_COMMON) gtkmm-3.0 epoxy cairomm-pdf-1.0 librsvg-2.0 libzmq libcurl libpng) -lpodofo -lTKHLR -lTKGeomBase -o $@
 
 $(BUILDDIR)/horizon-pool: $(OBJ_COMMON) $(OBJ_POOL_UTIL)
 	$(ECHO) " $@"
@@ -967,7 +967,7 @@ $(BUILDDIR)/horizon-prj: $(OBJ_COMMON) $(OBJ_PRJ_UTIL)
 
 $(BUILDDIR)/horizon-eda: $(OBJ_COMMON) $(OBJ_POOL_PRJ_MGR) $(OBJ_RES)
 	$(ECHO) " $@"
-	$(QUIET)$(CXX) $^ $(LDFLAGS) $(LDFLAGS_GUI) $(shell $(PKG_CONFIG) --libs $(LIBS_COMMON) gtkmm-3.0 epoxy libzmq libcurl libgit2) -o $@
+	$(QUIET)$(CXX) $^ $(LDFLAGS) $(LDFLAGS_GUI) $(shell $(PKG_CONFIG) --libs $(LIBS_COMMON) gtkmm-3.0 epoxy libzmq libcurl libgit2 libpng) -o $@
 
 $(BUILDDIR)/horizon-pgm-test: $(OBJ_COMMON) $(OBJ_PGM_TEST)
 	$(ECHO) " $@"
@@ -979,11 +979,11 @@ $(BUILDDIR)/horizon-gen-pkg: $(OBJ_COMMON) $(OBJ_GEN_PKG)
 
 $(BUILDDIR)/horizon-pr-review: $(OBJ_COMMON) $(OBJ_PR_REVIEW) $(OBJ_SHARED_3D)
 	$(ECHO) " $@"
-	$(QUIET)$(CXX) $^ $(LDFLAGS) $(INC) $(CXXFLAGS) $(shell $(PKG_CONFIG) --libs $(LIBS_COMMON) glibmm-2.4 giomm-2.4 cairomm-1.0 libgit2) -lOSMesa $(LDFLAGS_OCE) -o $@
+	$(QUIET)$(CXX) $^ $(LDFLAGS) $(INC) $(CXXFLAGS) $(shell $(PKG_CONFIG) --libs $(LIBS_COMMON) glibmm-2.4 giomm-2.4 cairomm-1.0 libgit2 libpng) -lOSMesa $(LDFLAGS_OCE) -o $@
 
 $(BUILDDIR)/horizon.so: $(OBJ_PYTHON) $(OBJ_SHARED) $(OBJ_SHARED_OCE)
 	$(ECHO) " $@"
-	$(QUIET)$(CXX) $^ $(LDFLAGS) $(INC) $(CXXFLAGS) $(shell $(PKG_CONFIG) --libs $(LIBS_COMMON) python3 glibmm-2.4 giomm-2.4 cairomm-1.0 py3cairo) -lpodofo  $(OCE_LIBDIRS) -lTKXDESTEP -lOSMesa -shared -o $@
+	$(QUIET)$(CXX) $^ $(LDFLAGS) $(INC) $(CXXFLAGS) $(shell $(PKG_CONFIG) --libs $(LIBS_COMMON) python3 glibmm-2.4 giomm-2.4 cairomm-1.0 py3cairo libpng) -lpodofo  $(OCE_LIBDIRS) -lTKXDESTEP -lOSMesa -shared -o $@
 
 $(OBJDIR)/%.o: %.c
 	$(QUIET)$(MKDIR) $(dir $@)
--- src/util/picture_load.cpp
+++ src/util/picture_load.cpp
@@ -2,16 +2,176 @@
 #include <glibmm/miscutils.h>
 #include <glibmm/fileutils.h>
 #include <giomm/file.h>
-#include <cairomm/cairomm.h>
 #include <set>
 #include "util/util.hpp"
 #include <algorithm>
+#include <png.h>
 
 namespace horizon {
 
 static std::string build_pic_filename(const std::string &dir, const UUID &uu, const std::string &suffix)
 {
     return Glib::build_filename(dir, (std::string)uu + "_" + suffix + ".png");
+}
+
+// adapted from https://gitlab.freedesktop.org/cairo/cairo/-/blob/master/src/cairo-png.c
+
+static void png_simple_error_callback(png_structp png, png_const_charp error_msg)
+{
+    throw std::runtime_error(std::string("png error: ") + error_msg);
+}
+
+static void png_simple_warning_callback(png_structp png, png_const_charp error_msg)
+{
+}
+
+struct Png {
+    png_struct *png = nullptr;
+    png_info *info = nullptr;
+};
+
+struct PngWrite : Png {
+    ~PngWrite()
+    {
+        if (png)
+            png_destroy_write_struct(&png, &info);
+    }
+};
+
+
+struct PngRead : Png {
+    ~PngRead()
+    {
+        if (png)
+            png_destroy_read_struct(&png, &info, NULL);
+    }
+};
+
+struct FileWrapper {
+    FileWrapper(const char *filename, const char *modes)
+    {
+#ifdef G_OS_WIN32
+        auto wfilename = reinterpret_cast<wchar_t *>(g_utf8_to_utf16(filename, -1, NULL, NULL, NULL));
+        auto wmodes = reinterpret_cast<wchar_t *>(g_utf8_to_utf16(modes, -1, NULL, NULL, NULL));
+        fp = _wfopen(wfilename, wmodes);
+        g_free(wfilename);
+        g_free(wmodes);
+#else
+        fp = fopen(filename, modes);
+#endif
+        if (!fp) {
+            throw std::runtime_error("fopen error");
+        }
+    }
+    FILE *fp = NULL;
+    ~FileWrapper()
+    {
+        if (fp)
+            fclose(fp);
+    }
+};
+
+static void convert_data_to_bytes(png_structp png, png_row_infop row_info, png_bytep data)
+{
+    unsigned int i;
+
+    for (i = 0; i < row_info->rowbytes; i += 4) {
+        uint8_t *b = &data[i];
+        uint32_t pixel;
+
+        memcpy(&pixel, b, sizeof(uint32_t));
+
+        b[2] = (pixel & 0x00ff'0000) >> 16;
+        b[1] = (pixel & 0x0000'ff00) >> 8;
+        b[0] = (pixel & 0x0000'00ff) >> 0;
+        b[3] = (pixel & 0xff00'0000) >> 24;
+    }
+}
+
+template <bool use_alpha> static void convert_bytes_to_data(png_structp png, png_row_infop row_info, png_bytep data)
+{
+    unsigned int i;
+
+    for (i = 0; i < row_info->rowbytes; i += 4) {
+        uint8_t *base = &data[i];
+        uint8_t red = base[2];
+        uint8_t green = base[1];
+        uint8_t blue = base[0];
+        uint8_t alpha = base[3];
+        uint32_t pixel;
+
+        pixel = (red << 16) | (green << 8) | (blue << 0);
+        if constexpr (use_alpha) {
+            pixel |= alpha << 24;
+        }
+        else {
+            pixel |= (0xff << 24);
+        }
+        memcpy(base, &pixel, sizeof(uint32_t));
+    }
+}
+
+static std::shared_ptr<PictureData> read_png(const std::string &filename, const UUID &uu)
+{
+    FileWrapper fp{filename.c_str(), "rb"};
+
+    int status = 0;
+    PngRead png;
+    png.png = png_create_read_struct(PNG_LIBPNG_VER_STRING, &status, png_simple_error_callback,
+                                     png_simple_warning_callback);
+    if (png.png == NULL) {
+        throw std::runtime_error("png error png");
+    }
+
+    png.info = png_create_info_struct(png.png);
+    if (png.info == NULL) {
+        throw std::runtime_error("png error info");
+    }
+
+    png_init_io(png.png, fp.fp);
+
+    png_read_info(png.png, png.info);
+    int depth, color_type, interlace;
+    png_uint_32 png_width, png_height;
+
+    png_get_IHDR(png.png, png.info, &png_width, &png_height, &depth, &color_type, &interlace, NULL, NULL);
+    if (status) { /* catch any early warnings */
+        throw std::runtime_error("png error header");
+    }
+
+    switch (color_type) {
+    case PNG_COLOR_TYPE_RGB_ALPHA:
+        png_set_read_user_transform_fn(png.png, convert_bytes_to_data<true>);
+        break;
+    case PNG_COLOR_TYPE_RGB:
+        png_set_read_user_transform_fn(png.png, convert_bytes_to_data<false>);
+        break;
+    default:
+        throw std::runtime_error("unsupported color type " + std::to_string(color_type));
+    }
+
+    if (depth != 8) {
+        throw std::runtime_error("unsupported color depth");
+    }
+
+    if (interlace != PNG_INTERLACE_NONE)
+        throw std::runtime_error("unsupported interlacing");
+
+    png_set_filler(png.png, 0xff, PNG_FILLER_AFTER);
+
+    std::vector<uint32_t> data;
+    data.resize(png_width * png_height);
+
+    std::vector<png_byte *> row_pointers;
+
+    for (size_t i = 0; i < png_height; i++) {
+        row_pointers.push_back(reinterpret_cast<png_byte *>(data.data() + (i * png_width)));
+    }
+
+    png_read_image(png.png, row_pointers.data());
+    png_read_end(png.png, png.info);
+
+    return std::make_shared<PictureData>(uu, png_width, png_height, std::move(data));
 }
 
 void pictures_load(const std::list<std::map<UUID, Picture> *> &pictures, const std::string &dir,
@@ -26,35 +186,59 @@ void pictures_load(const std::list<std::map<UUID, Picture> *> &pictures, const s
             else {
                 auto pic_filename = build_pic_filename(dir, it.second.data_uuid, suffix);
                 if (Glib::file_test(pic_filename, Glib::FILE_TEST_IS_REGULAR)) {
-                    auto surf = Cairo::ImageSurface::create_from_png(pic_filename);
-                    if (surf->get_format() != Cairo::FORMAT_ARGB32 && surf->get_format() != Cairo::FORMAT_RGB24) {
-                        throw std::runtime_error("pic must be ARGB32/RGB24");
-                    }
-                    bool has_alpha = surf->get_format() == Cairo::FORMAT_ARGB32;
-                    surf->flush();
-                    auto w = surf->get_width();
-                    auto h = surf->get_height();
-                    std::vector<uint32_t> vd;
-                    vd.resize(w * h);
-                    auto dp = surf->get_data();
-                    for (int y = 0; y < h; y++) {
-                        auto p = dp;
-                        for (int x = 0; x < w; x++) {
-                            if (has_alpha)
-                                vd[y * w + x] = (p[3] << 24) | (p[0] << 16) | (p[1] << 8) | p[2];
-                            else
-                                vd[y * w + x] = (0xff << 24) | (p[0] << 16) | (p[1] << 8) | p[2];
-                            p += 4;
-                        }
-                        dp += surf->get_stride();
-                    }
-                    auto data = std::make_shared<PictureData>(it.second.data_uuid, w, h, std::move(vd));
+                    auto data = read_png(pic_filename, it.second.data_uuid);
                     it.second.data = data;
                     pictures_loaded.emplace(it.second.data_uuid, data);
                 }
             }
         }
     }
+}
+
+static void write_png_to_file(const std::string &filename, const PictureData &data)
+{
+    /* PNG complains about "Image width or height is zero in IHDR" */
+    if (data.width == 0 || data.height == 0) {
+        throw std::runtime_error("image must not have zero width or height");
+    }
+
+    FileWrapper fp{filename.c_str(), "wb"};
+    PngWrite png;
+
+    int status = 0;
+    png.png = png_create_write_struct(PNG_LIBPNG_VER_STRING, &status, png_simple_error_callback,
+                                      png_simple_warning_callback);
+    if (png.png == NULL) {
+        throw std::runtime_error("png error");
+    }
+
+    png.info = png_create_info_struct(png.png);
+    if (png.info == NULL) {
+        throw std::runtime_error("png error");
+    }
+
+    png_init_io(png.png, fp.fp);
+
+    const int bpc = 8;
+    const int png_color_type = PNG_COLOR_TYPE_RGB_ALPHA;
+
+    png_set_IHDR(png.png, png.info, data.width, data.height, bpc, png_color_type, PNG_INTERLACE_NONE,
+                 PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+
+    png_color_16 white;
+    white.gray = (1 << bpc) - 1;
+    white.red = white.blue = white.green = white.gray;
+    png_set_bKGD(png.png, png.info, &white);
+
+    std::vector<const png_byte *> row_pointers;
+
+    for (size_t i = 0; i < data.height; i++) {
+        row_pointers.push_back(reinterpret_cast<const png_byte *>(data.data.data() + (i * data.width)));
+    }
+
+    png_set_rows(png.png, png.info, const_cast<png_byte **>(row_pointers.data()));
+    png_set_write_user_transform_fn(png.png, convert_data_to_bytes);
+    png_write_png(png.png, png.info, PNG_TRANSFORM_IDENTITY, NULL);
 }
 
 void pictures_save(const std::list<const std::map<UUID, Picture> *> &pictures, const std::string &pic_dir,
@@ -80,23 +264,7 @@ void pictures_save(const std::list<const std::map<UUID, Picture> *> &pictures, c
                 pictures_to_delete.erase(it.second.data->uuid);
                 auto pic_filename = build_pic_filename(pic_dir, it.second.data_uuid, suffix);
                 if (!Glib::file_test(pic_filename, Glib::FILE_TEST_IS_REGULAR)) {
-                    auto surf = Cairo::ImageSurface::create(Cairo::Format::FORMAT_ARGB32, data->width, data->height);
-                    auto dp = surf->get_data();
-                    surf->flush();
-                    for (unsigned int y = 0; y < data->height; y++) {
-                        auto p = dp;
-                        for (unsigned int x = 0; x < data->width; x++) {
-                            auto v = data->data[y * data->width + x];
-                            p[3] = (v >> 24) & 0xff;
-                            p[0] = (v >> 16) & 0xff;
-                            p[1] = (v >> 8) & 0xff;
-                            p[2] = (v >> 0) & 0xff;
-                            p += 4;
-                        }
-                        dp += surf->get_stride();
-                    }
-                    surf->mark_dirty();
-                    surf->write_to_png(pic_filename);
+                    write_png_to_file(pic_filename, *data);
                 }
             }
         }
