7c54653cc70ea0e65991a819ceeb3b90fb68bccb
Lukas K <lu@0x83.eu>
2021-06-20 23:04:09 +0200

    create top/bottom layer pair for new boards

--- src/board/board.cpp
+++ src/board/board.cpp
@@ -285,12 +285,19 @@ Board::Board(const UUID &uu, Block &bl) : uuid(uu), block(&bl), version(app_vers
 {
     rules.add_rule(RuleID::CLEARANCE_COPPER);
     rules.add_rule(RuleID::CLEARANCE_COPPER_OTHER);
-    rules.add_rule(RuleID::TRACK_WIDTH);
     rules.add_rule(RuleID::PLANE);
-    auto r = dynamic_cast<RuleTrackWidth *>(rules.get_rules_nc(RuleID::TRACK_WIDTH).begin()->second);
-    r->widths.emplace(std::piecewise_construct, std::forward_as_tuple(0), std::forward_as_tuple());
-    // TBD: inner layers
-    r->widths.emplace(std::piecewise_construct, std::forward_as_tuple(-100), std::forward_as_tuple());
+    {
+        rules.add_rule(RuleID::TRACK_WIDTH);
+        auto &r = dynamic_cast<RuleTrackWidth &>(*rules.get_rules_nc(RuleID::TRACK_WIDTH).begin()->second);
+        r.widths.emplace(std::piecewise_construct, std::forward_as_tuple(0), std::forward_as_tuple());
+        // TBD: inner layers
+        r.widths.emplace(std::piecewise_construct, std::forward_as_tuple(-100), std::forward_as_tuple());
+    }
+    {
+        rules.add_rule(RuleID::LAYER_PAIR);
+        auto &r = dynamic_cast<RuleLayerPair &>(*rules.get_rules_nc(RuleID::LAYER_PAIR).begin()->second);
+        r.layers = std::make_pair(BoardLayers::TOP_COPPER, BoardLayers::BOTTOM_COPPER);
+    }
 }
 
 Junction *Board::get_junction(const UUID &uu)
