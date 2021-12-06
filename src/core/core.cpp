horizon-eda/horizon

commit d8ba3ff1b3676001e6b38bcfe8e3d53e9335ef6f

Core: store what has been done in history

src/core/core.cpp
@@ -63,6 +63,7 @@ ToolResponse Core::tool_begin(ToolID tool_id, const ToolArgs &args, class ImpInt
                                  Logger::Domain::CORE, e.what());
             return ToolResponse::end();
         }
+        tool_id_current = tool_id;
         maybe_end_tool(r);
 
         return r;
@@ -82,15 +83,17 @@ void Core::maybe_end_tool(const ToolResponse &r)
         s_signal_tool_changed.emit(ToolID::NONE);
         if (r.result == ToolResponse::Result::COMMIT) {
             set_needs_save(true);
-            rebuild_internal(false);
+            const auto comment = action_catalog.at(tool_id_current).name;
+            rebuild_internal(false, comment);
         }
         else if (r.result == ToolResponse::Result::REVERT) {
             history_load(history_current);
-            rebuild_internal(true);
+            rebuild_internal(true, "undo");
         }
         else if (r.result == ToolResponse::Result::END) { // did nothing
             // do nothing
         }
+        tool_id_current = ToolID::NONE;
     }
 }
 
@@ -138,19 +141,19 @@ ToolResponse Core::tool_update(const ToolArgs &args)
     return ToolResponse();
 }
 
-void Core::rebuild()
+void Core::rebuild(const std::string &comment)
 {
-    rebuild_internal(false);
+    rebuild_internal(false, comment);
 }
 
-void Core::rebuild_finish(bool from_undo)
+void Core::rebuild_finish(bool from_undo, const std::string &comment)
 {
     if (!from_undo) {
         while (history_current + 1 != (int)history.size()) {
             history.pop_back();
         }
         assert(history_current + 1 == (int)history.size());
-        history_push();
+        history_push(comment);
         history_current++;
         history_trim();
     }
@@ -179,6 +182,28 @@ void Core::redo()
     signal_rebuilt().emit();
     signal_can_undo_redo().emit();
     set_needs_save();
+}
+
+static const std::string empty_string;
+
+const std::string &Core::get_undo_comment() const
+{
+    if (can_undo()) {
+        return history.at(history_current)->comment;
+    }
+    else {
+        return empty_string;
+    }
+}
+
+const std::string &Core::get_redo_comment() const
+{
+    if (can_redo()) {
+        return history.at(history_current + 1)->comment;
+    }
+    else {
+        return empty_string;
+    }
 }
 
 void Core::history_clear()
@@ -218,7 +243,7 @@ void Core::set_property_commit()
 {
     if (!property_transaction)
         throw std::runtime_error("no transaction in progress");
-    rebuild();
+    rebuild("edit property");
     set_needs_save(true);
     property_transaction = false;
 }
@@ -278,7 +303,8 @@ void Core::autosave()
     save(autosave_suffix);
 }
 
-Core::Core(IPool &pool, IPool *pool_caching) : m_pool(pool), m_pool_caching(pool_caching ? *pool_caching : pool)
+Core::Core(IPool &pool, IPool *pool_caching)
+    : m_pool(pool), m_pool_caching(pool_caching ? *pool_caching : pool), tool_id_current(ToolID::NONE)
 {
 }
 

src/core/core_board.cpp
@@ -53,7 +53,7 @@ CoreBoard::CoreBoard(const std::string &board_filename, const std::string &block
       m_blocks_filename(blocks_filename), m_pictures_dir(pictures_dir)
 {
     brd->load_pictures(pictures_dir);
-    rebuild();
+    rebuild("init");
 }
 
 void CoreBoard::reload_netlist()
@@ -98,7 +98,7 @@ void CoreBoard::reload_netlist()
     brd->update_refs();
     rules.cleanup(&*block);
     brd->expand_flags = Board::EXPAND_PROPAGATE_NETS | Board::EXPAND_ALL_AIRWIRES | Board::EXPAND_PACKAGES;
-    rebuild();
+    rebuild("reload netlist");
 }
 
 bool CoreBoard::get_property(ObjectType type, const UUID &uu, ObjectProperty::ID property, PropertyValue &value)
@@ -552,7 +552,7 @@ bool CoreBoard::set_property(ObjectType type, const UUID &uu, ObjectProperty::ID
         return false;
     }
     if (!property_transaction) {
-        rebuild_internal(false);
+        rebuild_internal(false, "edit properties");
         set_needs_save(true);
     }
     return true;
@@ -621,11 +621,11 @@ bool CoreBoard::get_property_meta(ObjectType type, const UUID &uu, ObjectPropert
     }
 }
 
-void CoreBoard::rebuild_internal(bool from_undo)
+void CoreBoard::rebuild_internal(bool from_undo, const std::string &comment)
 {
     clock_t begin = clock();
     brd->expand_some();
-    rebuild_finish(from_undo);
+    rebuild_finish(from_undo, comment);
     clock_t end = clock();
     double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
     std::cout << "rebuild took " << elapsed_secs << std::endl;
@@ -666,13 +666,14 @@ void CoreBoard::update_rules()
     brd->rules = rules;
 }
 
-CoreBoard::HistoryItem::HistoryItem(const Block &b, const Board &r) : block(b), brd(shallow_copy, r)
+CoreBoard::HistoryItem::HistoryItem(const Block &b, const Board &r, const std::string &cm)
+    : Core::HistoryItem(cm), block(b), brd(shallow_copy, r)
 {
 }
 
-void CoreBoard::history_push()
+void CoreBoard::history_push(const std::string &comment)
 {
-    history.push_back(std::make_unique<CoreBoard::HistoryItem>(*block, *brd));
+    history.push_back(std::make_unique<CoreBoard::HistoryItem>(*block, *brd, comment));
 }
 
 void CoreBoard::history_load(unsigned int i)
@@ -698,7 +699,7 @@ void CoreBoard::reload_pool()
     brd.emplace(brd->uuid, brd_j, *block, m_pool_caching);
     keeper.restore(brd->pictures);
     history_clear();
-    rebuild();
+    rebuild("reload pool");
 }
 
 json CoreBoard::get_meta()

src/core/core_package.cpp
@@ -16,7 +16,7 @@ CorePackage::CorePackage(const std::string &filename, IPool &pool)
       parameter_set(package.parameter_set), models(package.models), default_model(package.default_model)
 {
     package.load_pictures(m_pictures_dir);
-    rebuild();
+    rebuild("init");
 }
 
 bool CorePackage::has_object_type(ObjectType ty) const
@@ -188,7 +188,7 @@ bool CorePackage::set_property(ObjectType type, const UUID &uu, ObjectProperty::
         return false;
     }
     if (!property_transaction) {
-        rebuild_internal(false);
+        rebuild_internal(false, "edit properties");
         set_needs_save(true);
     }
     return true;
@@ -210,19 +210,19 @@ bool CorePackage::get_property_meta(ObjectType type, const UUID &uu, ObjectPrope
     return Core::get_property_meta(type, uu, property, meta);
 }
 
-void CorePackage::rebuild_internal(bool from_undo)
+void CorePackage::rebuild_internal(bool from_undo, const std::string &comment)
 {
     if (auto r = package.apply_parameter_set({})) {
         Logger::get().log_critical("Parameter program failed", Logger::Domain::CORE, r.value());
     }
     package.expand();
     package.update_warnings();
-    rebuild_finish(from_undo);
+    rebuild_finish(from_undo, comment);
 }
 
-void CorePackage::history_push()
+void CorePackage::history_push(const std::string &comment)
 {
-    history.push_back(std::make_unique<CorePackage::HistoryItem>(package));
+    history.push_back(std::make_unique<CorePackage::HistoryItem>(package, comment));
 }
 
 void CorePackage::history_load(unsigned int i)
@@ -241,7 +241,7 @@ void CorePackage::reload_pool()
     m_pool.clear();
     package.update_refs(m_pool);
     history_clear();
-    rebuild();
+    rebuild("reload pool");
 }
 
 json CorePackage::get_meta()

src/core/core_schematic.cpp
@@ -29,7 +29,7 @@ CoreSchematic::CoreSchematic(const std::string &blocks_filename, const std::stri
         block.schematic.load_pictures(m_pictures_dir);
         block.symbol.load_pictures(m_pictures_dir);
     }
-    rebuild();
+    rebuild("init");
 }
 
 Junction *CoreSchematic::get_junction(const UUID &uu)
@@ -493,7 +493,7 @@ bool CoreSchematic::set_property(ObjectType type, const UUID &uu, ObjectProperty
         return false;
     }
     if (!property_transaction) {
-        rebuild_internal(false);
+        rebuild_internal(false, "edit properties");
         set_needs_save(true);
     }
     return true;
@@ -669,7 +669,7 @@ void CoreSchematic::add_sheet()
     auto &sch = *get_current_schematic();
     auto &sheet = sch.add_sheet();
     sheet.pool_frame = sch.sheets.at(sheet_uuid).pool_frame;
-    rebuild();
+    rebuild("add sheet");
 }
 
 void CoreSchematic::delete_sheet(const UUID &uu)
@@ -680,7 +680,7 @@ void CoreSchematic::delete_sheet(const UUID &uu)
         if (sheet_uuid == uu) {       // deleted current sheet
             sheet_uuid = sch->get_sheet_at_index(1).uuid;
         }
-        rebuild();
+        rebuild("delete sheet");
     }
 }
 
@@ -846,7 +846,7 @@ void CoreSchematic::fix_current_block()
     }
 }
 
-void CoreSchematic::rebuild_internal(bool from_undo)
+void CoreSchematic::rebuild_internal(bool from_undo, const std::string &comment)
 {
     clock_t begin = clock();
     auto &top_block = blocks->get_top_block_item();
@@ -872,7 +872,7 @@ void CoreSchematic::rebuild_internal(bool from_undo)
             prv = this;
         block.schematic.expand(false, prv);
     }
-    rebuild_finish(from_undo);
+    rebuild_finish(from_undo, comment);
     clock_t end = clock();
     double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
     std::cout << "rebuild took " << elapsed_secs << std::endl;
@@ -888,9 +888,9 @@ const BlockSymbol &CoreSchematic::get_canvas_data_block_symbol()
     return get_block_symbol();
 }
 
-void CoreSchematic::history_push()
+void CoreSchematic::history_push(const std::string &comment)
 {
-    history.push_back(std::make_unique<CoreSchematic::HistoryItem>(*blocks));
+    history.push_back(std::make_unique<CoreSchematic::HistoryItem>(*blocks, comment));
 }
 
 void CoreSchematic::history_load(unsigned int i)
@@ -948,7 +948,7 @@ void CoreSchematic::reload_pool()
 
     bom_export_settings.update_refs(m_pool_caching);
     history_clear();
-    rebuild();
+    rebuild("reload pool");
 }
 
 std::pair<Coordi, Coordi> CoreSchematic::get_bbox()

src/core/core_symbol.cpp
@@ -12,7 +12,7 @@ namespace horizon {
 CoreSymbol::CoreSymbol(const std::string &filename, IPool &pool)
     : Core(pool, nullptr), sym(Symbol::new_from_file(filename, pool)), m_filename(filename)
 {
-    rebuild();
+    rebuild("init");
 }
 
 bool CoreSymbol::has_object_type(ObjectType ty) const
@@ -228,7 +228,7 @@ bool CoreSymbol::set_property(ObjectType type, const UUID &uu, ObjectProperty::I
         return false;
     }
     if (!property_transaction) {
-        rebuild_internal(false);
+        rebuild_internal(false, "edit properties");
         set_needs_save(true);
     }
     return true;
@@ -265,15 +265,15 @@ std::string CoreSymbol::get_display_name(ObjectType type, const UUID &uu)
     }
 }
 
-void CoreSymbol::rebuild_internal(bool from_undo)
+void CoreSymbol::rebuild_internal(bool from_undo, const std::string &comment)
 {
     sym.expand(pin_display_mode);
-    rebuild_finish(from_undo);
+    rebuild_finish(from_undo, comment);
 }
 
-void CoreSymbol::history_push()
+void CoreSymbol::history_push(const std::string &comment)
 {
-    history.push_back(std::make_unique<CoreSymbol::HistoryItem>(sym));
+    history.push_back(std::make_unique<CoreSymbol::HistoryItem>(sym, comment));
 }
 
 void CoreSymbol::history_load(unsigned int i)
@@ -316,7 +316,7 @@ void CoreSymbol::reload_pool()
     m_pool.clear();
     sym.unit = m_pool.get_unit(sym.unit.uuid);
     history_clear();
-    rebuild();
+    rebuild("reload pool");
 }
 
 void CoreSymbol::set_pin_display_mode(Symbol::PinDisplayMode mode)

src/core/core.hpp
@@ -70,7 +70,7 @@ public:
      * Expands the non-working document.
      * And copies the non-working document to the working document.
      */
-    void rebuild();
+    void rebuild(const std::string &comment);
     ToolResponse tool_begin(ToolID tool_id, const ToolArgs &args, class ImpInterface *imp, bool transient = false);
     ToolResponse tool_update(const ToolArgs &args);
     std::pair<bool, bool> tool_can_begin(ToolID tool_id, const std::set<SelectableRef> &selection);
@@ -83,6 +83,9 @@ public:
 
     bool can_undo() const;
     bool can_redo() const;
+
+    const std::string &get_undo_comment() const;
+    const std::string &get_redo_comment() const;
 
     inline bool tool_is_active()
     {
@@ -179,6 +182,7 @@ protected:
     class IPool &m_pool;
     class IPool &m_pool_caching;
 
+    ToolID tool_id_current;
     std::unique_ptr<ToolBase> tool = nullptr;
     type_signal_tool_changed s_signal_tool_changed;
     type_signal_rebuilt s_signal_rebuilt;
@@ -191,20 +195,21 @@ protected:
     bool needs_save = false;
     void set_needs_save(bool v);
 
-    void rebuild_finish(bool from_undo);
+    void rebuild_finish(bool from_undo, const std::string &comment);
 
     class HistoryItem {
     public:
-        // Symbol sym;
-        // HistoryItem(const Symbol &s): sym(s) {}
-        std::string comment;
+        HistoryItem(const std::string &c) : comment(c)
+        {
+        }
+        const std::string comment;
         virtual ~HistoryItem()
         {
         }
     };
     std::deque<std::unique_ptr<HistoryItem>> history;
     int history_current = -1;
-    virtual void history_push() = 0;
+    virtual void history_push(const std::string &comment) = 0;
     virtual void history_load(unsigned int i) = 0;
     void history_clear();
     void history_trim();
@@ -223,6 +228,6 @@ private:
     std::unique_ptr<ToolBase> create_tool(ToolID tool_id);
     std::set<SelectableRef> tool_selection;
     void maybe_end_tool(const ToolResponse &r);
-    virtual void rebuild_internal(bool from_undo) = 0;
+    virtual void rebuild_internal(bool from_undo, const std::string &comment) = 0;
 };
 } // namespace horizon

src/core/core_board.hpp
@@ -93,12 +93,12 @@ private:
 
     class HistoryItem : public Core::HistoryItem {
     public:
-        HistoryItem(const Block &b, const Board &r);
+        HistoryItem(const Block &b, const Board &r, const std::string &comment);
         Block block;
         Board brd;
     };
-    void rebuild_internal(bool from_undo) override;
-    void history_push() override;
+    void rebuild_internal(bool from_undo, const std::string &comment) override;
+    void history_push(const std::string &comment) override;
     void history_load(unsigned int i) override;
     void save(const std::string &suffix) override;
     void delete_autosave() override;

src/core/core_decal.cpp
@@ -8,7 +8,7 @@ namespace horizon {
 CoreDecal::CoreDecal(const std::string &decal_filename, IPool &pool)
     : Core(pool, nullptr), decal(Decal::new_from_file(decal_filename)), m_decal_filename(decal_filename)
 {
-    rebuild();
+    rebuild("init");
 }
 
 
@@ -52,9 +52,9 @@ std::map<UUID, Arc> *CoreDecal::get_arc_map()
     return &decal.arcs;
 }
 
-void CoreDecal::rebuild_internal(bool from_undo)
+void CoreDecal::rebuild_internal(bool from_undo, const std::string &comment)
 {
-    rebuild_finish(from_undo);
+    rebuild_finish(from_undo, comment);
 }
 
 LayerProvider &CoreDecal::get_layer_provider()
@@ -72,13 +72,13 @@ Decal &CoreDecal::get_decal()
     return decal;
 }
 
-CoreDecal::HistoryItem::HistoryItem(const Decal &dec) : decal(dec)
+CoreDecal::HistoryItem::HistoryItem(const Decal &dec, const std::string &cm) : Core::HistoryItem(cm), decal(dec)
 {
 }
 
-void CoreDecal::history_push()
+void CoreDecal::history_push(const std::string &comment)
 {
-    history.push_back(std::make_unique<CoreDecal::HistoryItem>(decal));
+    history.push_back(std::make_unique<CoreDecal::HistoryItem>(decal, comment));
 }
 
 void CoreDecal::history_load(unsigned int i)

commit f2c38e89d543aae027135142624ff0a7afb1a8f7

imp: don't crash on pool reload
only emit signal_can_undo_redo after rebuild

broken in d8ba3ff1b3676001e6b38bcfe8e3d53e9335ef6f

src/core/core.cpp
@@ -210,7 +210,6 @@ void Core::history_clear()
 {
     history.clear();
     history_current = -1;
-    signal_can_undo_redo().emit();
 }
 
 void Core::history_trim()

src/core/core_board.cpp
@@ -700,6 +700,7 @@ void CoreBoard::reload_pool()
     keeper.restore(brd->pictures);
     history_clear();
     rebuild("reload pool");
+    s_signal_can_undo_redo.emit();
 }
 
 json CoreBoard::get_meta()

src/core/core_package.cpp
@@ -242,6 +242,7 @@ void CorePackage::reload_pool()
     package.update_refs(m_pool);
     history_clear();
     rebuild("reload pool");
+    s_signal_can_undo_redo.emit();
 }
 
 json CorePackage::get_meta()

src/core/core_schematic.cpp
@@ -949,6 +949,7 @@ void CoreSchematic::reload_pool()
     bom_export_settings.update_refs(m_pool_caching);
     history_clear();
     rebuild("reload pool");
+    s_signal_can_undo_redo.emit();
 }
 
 std::pair<Coordi, Coordi> CoreSchematic::get_bbox()

src/core/core_symbol.cpp
@@ -317,6 +317,7 @@ void CoreSymbol::reload_pool()
     sym.unit = m_pool.get_unit(sym.unit.uuid);
     history_clear();
     rebuild("reload pool");
+    s_signal_can_undo_redo.emit();
 }
 
 void CoreSymbol::set_pin_display_mode(Symbol::PinDisplayMode mode)