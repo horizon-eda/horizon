9b7e68ed0385103a5469ca3eec8ccba3b4c44124
Lukas K <lu@0x83.eu>
2022-01-07 17:14:00 +0100

    version 2.1.91

--- scripts/make_changelog.py
+++ scripts/make_changelog.py
@@ -5,11 +5,14 @@ import pygit2
 repo = pygit2.Repository(".")
 
 def rep(match) :
-	commit_hash = match.group(0)
-	commit,_ = repo.resolve_refish(commit_hash)
-	return "[%s](https://github.com/horizon-eda/horizon/commit/%s)"%(commit.short_id, commit_hash)
+	m = match.group(0)
+	if m.startswith("@"):
+		return f"[{m}](https://github.com/{m[1:]})"
+	else :
+		commit,_ = repo.resolve_refish(m)
+		return "[%s](https://github.com/horizon-eda/horizon/commit/%s)"%(commit.short_id, m)
 
 with open("scripts/CHANGELOG.md.in", "r") as infile :
 	changelog = infile.read()
 	with open("CHANGELOG.md", "w") as outfile :
-		outfile.write(re.sub(r"\b[0-9a-f]{40}\b", rep, changelog))
+		outfile.write(re.sub(r"\b[0-9a-f]{40}|@\w+\b", rep, changelog))
--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 major = 2
 minor = 1
-micro = 90
+micro = 91
 name = "Green Flash"
 
 string = ".".join(str(x) for x in (major, minor, micro))
