a29d84f0506a775bdc1f8d1f96295aacb4599c2f
Lukas K <lu@0x83.eu>
2022-02-09 19:37:52 +0100

    RuleEditorShortedPads: allocate more space to match editors

--- src/imp/rules/rule_editor_shorted_pads.cpp
+++ src/imp/rules/rule_editor_shorted_pads.cpp
@@ -32,11 +32,13 @@ void RuleEditorShortedPads::populate()
     }
 
     auto match_component_editor = Gtk::manage(new RuleMatchComponentEditor(rule2->match_component, core));
+    match_component_editor->set_hexpand(true);
     match_component_editor->set_orientation(Gtk::ORIENTATION_HORIZONTAL);
     match_component_editor->signal_updated().connect([this] { s_signal_updated.emit(); });
     grid->attach(*match_component_editor, 0, 1, 1, 1);
 
     auto match_editor = Gtk::manage(new RuleMatchEditor(rule2->match, core));
+    match_editor->set_hexpand(true);
     match_editor->set_orientation(Gtk::ORIENTATION_HORIZONTAL);
     match_editor->signal_updated().connect([this] { s_signal_updated.emit(); });
     grid->attach(*match_editor, 1, 1, 1, 1);
