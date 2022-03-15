c472863f9f39f42e99c0be946bba9010471a4751
Lukas K <lu@0x83.eu>
2022-03-15 22:21:23 +0100

    warp_cursor: return early

--- src/util/warp_cursor.cpp
+++ src/util/warp_cursor.cpp
@@ -6,6 +6,12 @@ Coordi warp_cursor(GdkEvent *motion_event, const Gtk::Widget &widget)
     gdouble x, y;
     if (!gdk_event_get_coords(motion_event, &x, &y))
         return {};
+    auto dev = gdk_event_get_device(motion_event);
+    auto srcdev = gdk_event_get_source_device(motion_event);
+    const auto src = gdk_device_get_source(srcdev);
+    if (src == GDK_SOURCE_PEN)
+        return {};
+    auto scr = gdk_event_get_screen(motion_event);
     const auto w = widget.get_allocated_width();
     const auto h = widget.get_allocated_height();
     const int offset = 1;
@@ -15,27 +21,21 @@ Coordi warp_cursor(GdkEvent *motion_event, const Gtk::Widget &widget)
     const bool wt = y <= offset;
     Coordi warp_distance;
     if (wr || wl || wb || wt) {
-        auto dev = gdk_event_get_device(motion_event);
-        auto srcdev = gdk_event_get_source_device(motion_event);
-        const auto src = gdk_device_get_source(srcdev);
-        if (src != GDK_SOURCE_PEN) {
-            auto scr = gdk_event_get_screen(motion_event);
-            gdouble rx, ry;
-            gdk_event_get_root_coords(motion_event, &rx, &ry);
-            if (wr) {
-                warp_distance = Coordi(-(w - 2 * offset), 0);
-            }
-            else if (wl) {
-                warp_distance = Coordi(+(w - 2 * offset), 0);
-            }
-            else if (wb) {
-                warp_distance = Coordi(0, -(h - 2 * offset));
-            }
-            else if (wt) {
-                warp_distance = Coordi(0, h - 2 * offset);
-            }
-            gdk_device_warp(dev, scr, rx + warp_distance.x, ry + warp_distance.y);
+        gdouble rx, ry;
+        gdk_event_get_root_coords(motion_event, &rx, &ry);
+        if (wr) {
+            warp_distance = Coordi(-(w - 2 * offset), 0);
         }
+        else if (wl) {
+            warp_distance = Coordi(+(w - 2 * offset), 0);
+        }
+        else if (wb) {
+            warp_distance = Coordi(0, -(h - 2 * offset));
+        }
+        else if (wt) {
+            warp_distance = Coordi(0, h - 2 * offset);
+        }
+        gdk_device_warp(dev, scr, rx + warp_distance.x, ry + warp_distance.y);
     }
     return warp_distance;
 }
