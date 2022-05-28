c23a39607cd4e96a78d34260fa5540c9a458a5f2
Lukas K <lu@0x83.eu>
2022-05-28 17:14:46 +0200

    fs_util: don't use std::search on fs:path::iterator
    apparently, it's not standard-compliant, see https://github.com/llvm/llvm-project/issues/55683
    
    closes #666

--- src/util/fs_util.cpp
+++ src/util/fs_util.cpp
@@ -8,10 +8,10 @@ std::optional<std::string> get_relative_filename(const std::string &path, const 
 {
     const auto p = fs::u8path(path);
     const auto b = fs::u8path(base);
-    const auto x = std::search(p.begin(), p.end(), b.begin(), b.end());
-    if (x == p.begin()) // path is in base path
-        return fs::relative(p, b).u8string();
-    else
-        return {};
+    for (auto ip = p.begin(), ib = b.begin(); ib != b.end(); ip++, ib++) {
+        if (ip == p.end() || *ip != *ib)
+            return {};
+    }
+    return fs::relative(p, b).u8string();
 }
 } // namespace horizon
