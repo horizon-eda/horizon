horizon-eda/horizon

commit de299b785623da30e9ddaad6b54aa4877f880e9f

update kicad router to 6.0.4
adds support for arc tracks

src/canvas/shaders/triangle-ubo.glsl
@@ -1,6 +1,6 @@
 layout (std140) uniform layer_setup
 {
-	vec3 colors[21];
+	vec3 colors[22];
 	vec3 colors2[256];
 	mat3 screenmat;
 	mat3 viewmat;

src/canvas/triangle_renderer.cpp
@@ -143,7 +143,7 @@ void TriangleRenderer::realize()
 
 class UBOBuffer {
 public:
-    static constexpr size_t ubo_size = 21; //  keep in sync with ubo.glsl
+    static constexpr size_t ubo_size = 22; //  keep in sync with ubo.glsl
     static_assert(static_cast<int>(ColorP::N_COLORS) == ubo_size, "ubo size mismatch");
     std::array<std::array<float, 4>, ubo_size> colors;
     std::array<std::array<float, 4>, 256> colors2; // keep in sync with shader
@@ -295,7 +295,8 @@ void TriangleRenderer::render_layer_batch(int layer, HighlightMode highlight_mod
             buf.colors[static_cast<int>(ColorP::AIRWIRE_ROUTER)] =
                     gl_array_from_color(ca.appearance.colors.at(ColorP::AIRWIRE_ROUTER));
             buf.colors[static_cast<int>(ColorP::FROM_LAYER)] = apply_highlight(lc, highlight_mode, layer);
-            buf.colors[static_cast<int>(ColorP::LAYER_HIGHLIGHT)] =
+            buf.colors[static_cast<int>(ColorP::LAYER_HIGHLIGHT)] = apply_highlight(lc, highlight_mode, layer);
+            buf.colors[static_cast<int>(ColorP::LAYER_HIGHLIGHT_LIGHTEN)] =
                     gl_array_from_color(lc) + ca.appearance.highlight_lighten;
             for (size_t i = 0; i < std::min(buf.colors2.size(), ca.colors2.size()); i++) {
                 buf.colors2[i] = apply_highlight(ca.colors2[i].to_color(), highlight_mode, layer);
@@ -489,7 +490,8 @@ void TriangleRenderer::push()
                     throw std::runtime_error("unknown triangle type");
                 }
                 const bool highlight = (tri_info.flags & TriangleInfo::FLAG_HIGHLIGHT)
-                                       || (tri.color == static_cast<int>(ColorP::LAYER_HIGHLIGHT));
+                                       || (tri.color == static_cast<int>(ColorP::LAYER_HIGHLIGHT))
+                                       || (tri.color == static_cast<int>(ColorP::LAYER_HIGHLIGHT_LIGHTEN));
                 const bool do_stencil = tri_info.type == TriangleInfo::Type::PAD;
                 const BatchKey key{ty, highlight, do_stencil};
                 type_indices[key].push_back(i + ofs);

commit fe0cc861949957cb3a465f3312b090e9266240ee

fix setting colors of airwires
and make sure it doesn't break again

got broken by de299b785623da30e9ddaad6b54aa4877f880e9f as it changed the
number of the airwire color

src/canvas/shaders/triangle-ubo.glsl
@@ -43,7 +43,7 @@ bool get_discard(vec2 fc) {
 }
 
 vec3 get_color(int color, int color2) {
-	if(color2 != 0 && (color == 0 || color == 12 /*airwire*/)) {
+	if(color2 != 0 && (color == 0 || color == 13 /*airwire*/)) {
 		return colors2[color2];
 	}
 	else {

src/canvas/triangle_renderer.cpp
@@ -6,6 +6,10 @@
 
 namespace horizon {
 
+#define CHECK_ENUM_VAL(v, i) static_assert(static_cast<int>(v) == (i))
+CHECK_ENUM_VAL(ColorP::AIRWIRE, 13);
+#undef CHECK_ENUM_VAL
+
 static GLuint create_vao(GLuint program, GLuint &vbo_out, GLuint &ebo_out)
 {
     GLuint p0_index = 0;