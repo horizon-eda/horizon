horizon-eda/horizon

commit 49d41516948c4e72adc6244fcfedeeb370e7052f

pdf export: project arc centers in polygons onto perpendicular bisector
changelog: Bugfixes/Board Editor: correctly represent polygon arcs in
PDF export if the arc's center isn't on the perpendicular bisector

src/export_pdf/canvas_pdf.cpp
@@ -283,7 +283,7 @@ void CanvasPDF::draw_polygon(const Polygon &ipoly, bool tr)
         }
         else if (it->type == Polygon::Vertex::Type::ARC) {
             Coordd end = it_next->position;
-            Coordd c = it->arc_center;
+            Coordd c = project_onto_perp_bisector(end, p, it->arc_center);
             if (!first)
                 painter.LineToMM(to_um(p.x), to_um(p.y));
 

commit 1da59c19ba5b94c0eada691281db014d8cf39c53

pdf export: fix transformed polygons with arcs
got broken by 49d41516948c4e72adc6244fcfedeeb370e7052f

src/export_pdf/canvas_pdf.cpp
@@ -283,7 +283,7 @@ void CanvasPDF::draw_polygon(const Polygon &ipoly, bool tr)
         }
         else if (it->type == Polygon::Vertex::Type::ARC) {
             Coordd end = it_next->position;
-            Coordd c = project_onto_perp_bisector(end, p, it->arc_center);
+            Coordd c = project_onto_perp_bisector(end, it->position, it->arc_center);
             if (!first)
                 painter.LineToMM(to_um(p.x), to_um(p.y));
 