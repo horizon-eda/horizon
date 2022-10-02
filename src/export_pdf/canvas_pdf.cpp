horizon-eda/horizon

commit 16ac7d984809b6b3e106ecf5bd142ed2e067aa0f

Fix numerical errors with exported pdf arcs (#690)
The PdfPainterMM API only supports micrometer precision, however that's apparently not sufficient for reproducing small arcs/circles. Use PdfPainter instead as it supports floating point coordinates.

changelog: Bugfixes/Board Editor: correctly export small arcs as encountered on padstacks with rounded corners

src/export_pdf/canvas_pdf.cpp
@@ -10,7 +10,7 @@
 namespace horizon {
 
 
-CanvasPDF::CanvasPDF(PoDoFo::PdfPainterMM &p, PoDoFo::PdfFont &f, const PDFExportSettings &s)
+CanvasPDF::CanvasPDF(PoDoFo::PdfPainter &p, PoDoFo::PdfFont &f, const PDFExportSettings &s)
     : Canvas::Canvas(), painter(p), font(f), settings(s), metrics(font.GetFontMetrics())
 {
     img_mode = true;
@@ -40,7 +40,7 @@ void CanvasPDF::img_line(const Coordi &p0, const Coordi &p1, const uint64_t widt
         return;
     painter.Save();
     auto w = std::max(width, settings.min_line_width);
-    painter.SetStrokeWidthMM(to_um(w));
+    painter.SetStrokeWidth(to_pt(w));
     Coordi rp0 = p0;
     Coordi rp1 = p1;
     if (tr) {
@@ -49,7 +49,7 @@ void CanvasPDF::img_line(const Coordi &p0, const Coordi &p1, const uint64_t widt
     }
     auto color = get_pdf_layer_color(layer);
     painter.SetStrokingColor(color.r, color.g, color.b);
-    painter.DrawLineMM(to_um(rp0.x), to_um(rp0.y), to_um(rp1.x), to_um(rp1.y));
+    painter.DrawLine(to_pt(rp0.x), to_pt(rp0.y), to_pt(rp1.x), to_pt(rp1.y));
     painter.Restore();
 }
 
@@ -126,7 +126,7 @@ void CanvasPDF::img_draw_text(const Coordf &p, float size, const std::string &rt
 
         painter.SetTransformationMatrix(cos(fangle), sin(fangle), -sin(fangle), cos(fangle), to_pt(pt.x), to_pt(pt.y));
         PoDoFo::PdfString pstr(reinterpret_cast<const PoDoFo::pdf_utf8 *>(line.c_str()));
-        painter.DrawTextMM(0, to_um(size) / -2, pstr);
+        painter.DrawText(0, to_pt(size) / -2, pstr);
         painter.Restore();
 
         i_line++;
@@ -141,7 +141,7 @@ void CanvasPDF::img_polygon(const Polygon &ipoly, bool tr)
     auto color = get_pdf_layer_color(ipoly.layer);
     painter.SetColor(color.r, color.g, color.b);
     painter.SetStrokingColor(color.r, color.g, color.b);
-    painter.SetStrokeWidthMM(to_um(settings.min_line_width));
+    painter.SetStrokeWidth(to_pt(settings.min_line_width));
     if (ipoly.usage == nullptr) { // regular patch
         draw_polygon(ipoly, tr);
         if (fill)
@@ -158,9 +158,9 @@ void CanvasPDF::img_polygon(const Polygon &ipoly, bool tr)
                     if (tr)
                         p = transform.transform(p);
                     if (first)
-                        painter.MoveToMM(to_um(p.x), to_um(p.y));
+                        painter.MoveTo(to_pt(p.x), to_pt(p.y));
                     else
-                        painter.LineToMM(to_um(p.x), to_um(p.y));
+                        painter.LineTo(to_pt(p.x), to_pt(p.y));
                     first = false;
                 }
                 painter.ClosePath();
@@ -183,7 +183,7 @@ void CanvasPDF::img_hole(const Hole &hole)
     auto color = get_pdf_layer_color(PDFExportSettings::HOLES_LAYER);
     painter.SetColor(color.r, color.g, color.b);
     painter.SetStrokingColor(color.r, color.g, color.b);
-    painter.SetStrokeWidthMM(to_um(settings.min_line_width));
+    painter.SetStrokeWidth(to_pt(settings.min_line_width));
 
     auto hole2 = hole;
     if (settings.set_holes_size) {
@@ -198,7 +198,7 @@ void CanvasPDF::img_hole(const Hole &hole)
 }
 
 // c is the arc center.
-// angles must be in radians, c and r must be in a PDF "pt" unit.
+// angles must be in radians, c and r must be in mm.
 // See "How to determine the control points of a Bézier curve that approximates a
 // small circular arc" by Richard ADeVeneza, Nov 2004
 // https://www.tinaja.com/glib/bezcirc2.pdf
@@ -223,14 +223,13 @@ static Coordd pdf_arc_segment(PoDoFo::PdfPainter &painter, const Coordd c, const
     const auto c2 = p2.rotate(theta) * r + c;
     const auto c3 = p3.rotate(theta) * r + c;
 
-    painter.CubicBezierTo(c1.x, c1.y, c2.x, c2.y, c3.x, c3.y);
+    painter.CubicBezierTo(to_pt(c1.x), to_pt(c1.y), to_pt(c2.x), to_pt(c2.y), to_pt(c3.x), to_pt(c3.y));
     return c3; // end point
 }
 
 static void pdf_arc(PoDoFo::PdfPainter &painter, const Coordd start, const Coordd c, const Coordd end, bool cw)
 {
-    const auto r = to_pt((start - c).mag());
-    const auto ctr = Coordd(to_pt(c.x), to_pt(c.y));
+    const auto r = (start - c).mag();
 
     // Get angles relative to the x axis
     double a0 = (start - c).angle();
@@ -252,10 +251,10 @@ static void pdf_arc(PoDoFo::PdfPainter &painter, const Coordd start, const Coord
         assert(a0 < a1);
     }
     double e = a1 - a0;
-    while (std::abs(e) > 0) {
+    while (std::abs(e) > 1e-6) {
         const auto d = (cw) ? std::max(e, da) : std::min(e, da);
         const auto a = a0 + d;
-        pdf_arc_segment(painter, ctr, r, a0, a);
+        pdf_arc_segment(painter, c, r, a0, a);
         a0 = a;
         e = a1 - a0;
     }
@@ -275,17 +274,18 @@ void CanvasPDF::draw_polygon(const Polygon &ipoly, bool tr)
             it_next = ipoly.vertices.cbegin();
         }
         if (first) {
-            painter.MoveToMM(to_um(p.x), to_um(p.y));
+            painter.MoveTo(to_pt(p.x), to_pt(p.y));
         }
         if (it->type == Polygon::Vertex::Type::LINE) {
-            if (!first)
-                painter.LineToMM(to_um(p.x), to_um(p.y));
+            if (!first) {
+                painter.LineTo(to_pt(p.x), to_pt(p.y));
+            }
         }
         else if (it->type == Polygon::Vertex::Type::ARC) {
             Coordd end = it_next->position;
             Coordd c = project_onto_perp_bisector(end, it->position, it->arc_center);
             if (!first)
-                painter.LineToMM(to_um(p.x), to_um(p.y));
+                painter.LineTo(to_pt(p.x), to_pt(p.y));
 
             if (tr) {
                 c = transform.transform(c);

src/export_pdf/canvas_pdf.hpp
@@ -4,11 +4,6 @@
 
 namespace horizon {
 
-template <typename T> static T to_um(T x)
-{
-    return x / 1000;
-}
-
 template <typename T> static T to_pt(T x)
 {
     return x * .000002834645669291339;
@@ -16,7 +11,7 @@ template <typename T> static T to_pt(T x)
 
 class CanvasPDF : public Canvas {
 public:
-    CanvasPDF(PoDoFo::PdfPainterMM &painter, PoDoFo::PdfFont &font, const class PDFExportSettings &settings);
+    CanvasPDF(PoDoFo::PdfPainter &painter, PoDoFo::PdfFont &font, const class PDFExportSettings &settings);
     void push() override
     {
     }
@@ -32,7 +27,7 @@ public:
     }
 
 private:
-    PoDoFo::PdfPainterMM &painter;
+    PoDoFo::PdfPainter &painter;
     PoDoFo::PdfFont &font;
     const PDFExportSettings &settings;
     const PoDoFo::PdfFontMetrics *metrics;

src/export_pdf/export_pdf.cpp
@@ -112,7 +112,7 @@ public:
 
 private:
     PoDoFo::PdfStreamedDocument document;
-    PoDoFo::PdfPainterMM painter;
+    PoDoFo::PdfPainter painter;
     PoDoFo::PdfFont *font = nullptr;
     std::map<UUIDVec, PoDoFo::PdfDestination> first_pages;
     std::vector<std::tuple<UUIDVec, unsigned int, PoDoFo::PdfRect>> annotations;

src/export_pdf/export_pdf_board.cpp
@@ -19,7 +19,8 @@ void export_pdf(const class Board &brd, const class PDFExportSettings &settings,
     cb("Initializing", 0);
 
     PoDoFo::PdfStreamedDocument document(settings.output_filename.c_str());
-    PoDoFo::PdfPainterMM painter;
+    PoDoFo::PdfPainter painter;
+    painter.SetPrecision(9);
     auto info = document.GetInfo();
     info->SetCreator("horizon EDA");
     info->SetProducer("horizon EDA");

src/export_pdf/export_pdf_util.cpp
@@ -4,7 +4,7 @@
 
 namespace horizon {
 
-void render_picture(PoDoFo::PdfDocument &doc, PoDoFo::PdfPainterMM &painter, const Picture &pic, const Placement &tr)
+void render_picture(PoDoFo::PdfDocument &doc, PoDoFo::PdfPainter &painter, const Picture &pic, const Placement &tr)
 {
     PoDoFo::PdfImage img(&doc);
     Placement pl = tr;
@@ -48,7 +48,7 @@ void render_picture(PoDoFo::PdfDocument &doc, PoDoFo::PdfPainterMM &painter, con
     const int64_t h = pic.data->height * pic.px_size;
     const auto p = Coordd(w, h) / -2;
     const double sz = pic.px_size / (1e3 / CONVERSION_CONSTANT);
-    painter.DrawImageMM(to_um(p.x), to_um(p.y), &img, sz, sz);
+    painter.DrawImage(to_pt(p.x), to_pt(p.y), &img, sz, sz);
     painter.Restore();
 }
 } // namespace horizon

src/export_pdf/export_pdf_util.hpp
@@ -3,6 +3,6 @@
 #include "util/placement.hpp"
 
 namespace horizon {
-void render_picture(PoDoFo::PdfDocument &doc, PoDoFo::PdfPainterMM &painter, const class Picture &pic,
+void render_picture(PoDoFo::PdfDocument &doc, PoDoFo::PdfPainter &painter, const class Picture &pic,
                     const Placement &tr = Placement());
 }

commit 7d9959782cc07f781528e75c8ecad77fae6d0d3b

PDF export: fix precision for lines
probably got broken in 16ac7d984809b6b3e106ecf5bd142ed2e067aa0f

src/export_pdf/canvas_pdf.cpp
@@ -9,6 +9,10 @@
 
 namespace horizon {
 
+double to_pt(double x_nm)
+{
+    return x_nm * .000002834645669291339;
+}
 
 CanvasPDF::CanvasPDF(PoDoFo::PdfPainter &p, PoDoFo::PdfFont &f, const PDFExportSettings &s)
     : Canvas::Canvas(), painter(p), font(f), settings(s), metrics(font.GetFontMetrics())

src/export_pdf/canvas_pdf.hpp
@@ -4,10 +4,7 @@
 
 namespace horizon {
 
-template <typename T> static T to_pt(T x)
-{
-    return x * .000002834645669291339;
-}
+double to_pt(double x_nm);
 
 class CanvasPDF : public Canvas {
 public: