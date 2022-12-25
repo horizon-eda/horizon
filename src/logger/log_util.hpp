272b4daa653722db109d1e3cb9eb7618134a4bb3
Lukas K <lu@0x83.eu>
2022-12-25 18:55:33 +0100

    log util: allow loading of things with no object type

--- src/logger/log_util.hpp
+++ src/logger/log_util.hpp
@@ -11,16 +11,22 @@ template <typename T, typename... Args1>
 void load_and_log(std::map<UUID, T> &map, ObjectType type, std::tuple<Args1...> args,
                   Logger::Domain dom = Logger::Domain::UNSPECIFIED)
 {
+    load_and_log(map, object_descriptions.at(type).name, std::forward<std::tuple<Args1...>>(args), dom);
+}
+
+template <typename T, typename... Args1>
+void load_and_log(std::map<UUID, T> &map, const std::string &type, std::tuple<Args1...> args,
+                  Logger::Domain dom = Logger::Domain::UNSPECIFIED)
+{
     auto uu = std::get<0>(args);
     try {
         map.emplace(std::piecewise_construct, std::forward_as_tuple(uu), std::forward<std::tuple<Args1...>>(args));
     }
     catch (const std::exception &e) {
-        Logger::log_warning("couldn't load " + object_descriptions.at(type).name + " " + (std::string)uu, dom,
-                            e.what());
+        Logger::log_warning("couldn't load " + type + " " + (std::string)uu, dom, e.what());
     }
     catch (...) {
-        Logger::log_warning("couldn't load " + object_descriptions.at(type).name + " " + (std::string)uu, dom);
+        Logger::log_warning("couldn't load " + type + " " + (std::string)uu, dom);
     }
 }
 } // namespace horizon
