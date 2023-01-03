horizon-eda/horizon

commit 1c2c7134939a5ff9a1c1d875190c945cf7e37bf8

gerber output: pass plane fragments through polytree
changelog: Bugfixes/Board Editor: properly export gerber for plane
fragments in holes of enclosed planes

To trigger this bug, this conditions are needed: a plane consiting of
one fragment with a hole in it surrounded by another plane and that
plane having a fragment inside of the hole of the first plane.

The outer plane must have a higher fill order than the inner plane.

Gerber's way of representing holes in polyons is by drawing using 'light
polarity', i.e. erasing everything below the hole. To account for this,
planes are added to the gerber image highest fill order first. In this
particular case, that'd go like this:

 - Outer fragment of the outer plane
 - Hole of the outer plane
 - Fragment inside of the hole of the outer plane
 - Ring of the inner plane
 - Hole of the inner plane

The bug is that the hole of the inner plane erases the fragment of the
outer plane.

To fix this, we need to draw things in the right order. This is
accomplished using clipper's polytree so that all holes get drawn before
fragments contained within them, regardless which plane they belong to.

src/export_gerber/canvas_gerber.cpp
@@ -53,11 +53,7 @@ void CanvasGerber::img_polygon(const Polygon &ipoly, bool tr)
     else if (auto plane = dynamic_cast<const Plane *>(ipoly.usage.ptr)) {
         if (GerberWriter *wr = exporter.get_writer_for_layer(ipoly.layer)) {
             for (const auto &frag : plane->fragments) {
-                bool dark = true; // first path ist outline, the rest are holes
-                for (const auto &path : frag.paths) {
-                    wr->draw_region(transform_path(transform, path), dark, plane->priority);
-                    dark = false;
-                }
+                wr->draw_fragments(frag.paths);
             }
         }
     }
@@ -78,7 +74,7 @@ void CanvasGerber::img_polygon(const Polygon &ipoly, bool tr)
                                }
                                return ClipperLib::IntPoint(p.x, p.y);
                            });
-            wr->draw_region(path, true, -1);
+            wr->draw_polygon(path);
         }
     }
 }

src/export_gerber/gerber_writer.cpp
@@ -215,27 +215,62 @@ void GerberWriter::write_pads()
     }
 }
 
+void GerberWriter::draw_polygon(const ClipperLib::Path &path)
+{
+    polygons.emplace_back(path);
+}
+
+void GerberWriter::draw_fragments(const ClipperLib::Paths &paths)
+{
+    fragments.insert(fragments.end(), paths.begin(), paths.end());
+}
+
+void GerberWriter::write_path(const ClipperLib::Path &path)
+{
+    write_line("G36*");
+    ofs << Coordi(path.back().X, path.back().Y) << "D02*"
+        << "\r\n";
+    for (const auto &pt : path) {
+        ofs << Coordi(pt.X, pt.Y) << "D01*"
+            << "\r\n";
+    }
+    write_line("D02*");
+    write_line("G37*");
+}
+
+void GerberWriter::write_polynode(const ClipperLib::PolyNode *node)
+{
+    assert(node->IsHole() == false);
+
+    write_line("%LPD*%");
+    write_path(node->Contour);
+
+    for (auto child : node->Childs) {
+        assert(child->IsHole() == true);
+        write_line("%LPC*%");
+        write_path(child->Contour);
+
+        for (auto child2 : child->Childs) { // add fragments in holes
+            write_polynode(child2);
+        }
+    }
+}
+
 void GerberWriter::write_regions()
 {
     write_line("G01*");
-    std::stable_sort(regions.begin(), regions.end(),
-                     [](const auto &a, const auto &b) { return a.priority > b.priority; });
-    for (const auto &it : regions) {
-        if (it.dark) {
-            write_line("%LPD*%");
-        }
-        else {
-            write_line("%LPC*%");
-        }
-        write_line("G36*");
-        ofs << Coordi(it.path.back().X, it.path.back().Y) << "D02*"
-            << "\r\n";
-        for (const auto &pt : it.path) {
-            ofs << Coordi(pt.X, pt.Y) << "D01*"
-                << "\r\n";
-        }
-        write_line("D02*");
-        write_line("G37*");
+    ClipperLib::Clipper clipper;
+
+    clipper.AddPaths(fragments, ClipperLib::ptSubject, true);
+    ClipperLib::PolyTree tree;
+    clipper.Execute(ClipperLib::ctUnion, tree, ClipperLib::pftNonZero);
+    for (const auto node : tree.Childs) {
+        write_polynode(node);
+    }
+
+    write_line("%LPD*%");
+    for (const auto &poly : polygons) {
+        write_path(poly);
     }
 }
 
@@ -249,11 +284,6 @@ void GerberWriter::draw_arc(const Coordi &from, const Coordi &to, const Coordi &
 {
     auto ap = get_or_create_aperture_circle(width);
     arcs.emplace_back(from, to, project_onto_perp_bisector(from, to, center).to_coordi(), flip, ap);
-}
-
-void GerberWriter::draw_region(const ClipperLib::Path &path, bool dark, int prio)
-{
-    regions.emplace_back(path, dark, prio);
 }
 
 void GerberWriter::draw_padstack(const Padstack &ps, int layer, const Placement &transform)

src/export_gerber/gerber_writer.hpp
@@ -28,7 +28,8 @@ public:
     void draw_line(const Coordi &from, const Coordi &to, uint64_t width);
     void draw_arc(const Coordi &from, const Coordi &to, const Coordi &center, bool flip, uint64_t width);
     void draw_padstack(const Padstack &ps, int layer, const Placement &transform);
-    void draw_region(const ClipperLib::Path &path, bool dark = true, int prio = 0);
+    void draw_polygon(const ClipperLib::Path &path);
+    void draw_fragments(const ClipperLib::Paths &paths);
     const std::string &get_filename();
 
 private:
@@ -116,10 +117,13 @@ private:
 
     std::deque<Line> lines;
     std::deque<Arc> arcs;
-    std::deque<Region> regions;
+    ClipperLib::Paths fragments;
+    std::deque<ClipperLib::Path> polygons;
     std::deque<std::pair<unsigned int, Coordi>> pads;
     void write_decimal(int64_t x, bool comma = true);
     void write_prim(const ApertureMacro::PrimitiveOutline *prim);
     void write_prim(const ApertureMacro::PrimitiveCenterLine *prim);
+    void write_polynode(const ClipperLib::PolyNode *node);
+    void write_path(const ClipperLib::Path &path);
 };
 } // namespace horizon

commit b29af1995a6dc095eb3afa0b4898acc5720dafff

transform planes in gerber export
got broken by 1c2c7134939a5ff9a1c1d875190c945cf7e37bf8

changelog: Bugfixes/Board Editor: correctly export planes in panels

src/export_gerber/canvas_gerber.cpp
@@ -53,7 +53,7 @@ void CanvasGerber::img_polygon(const Polygon &ipoly, bool tr)
     else if (auto plane = dynamic_cast<const Plane *>(ipoly.usage.ptr)) {
         if (GerberWriter *wr = exporter.get_writer_for_layer(ipoly.layer)) {
             for (const auto &frag : plane->fragments) {
-                wr->draw_fragments(frag.paths);
+                wr->draw_fragments(transform_paths(transform, frag.paths));
             }
         }
     }

src/util/clipper_util.cpp
@@ -17,4 +17,13 @@ ClipperLib::Path transform_path(const Placement &transform, const ClipperLib::Pa
                    [transform](const auto &x) { return transform_intpt(transform, x); });
     return r;
 }
+
+ClipperLib::Paths transform_paths(const Placement &transform, const ClipperLib::Paths &paths)
+{
+    ClipperLib::Paths r;
+    r.reserve(paths.size());
+    std::transform(paths.begin(), paths.end(), std::back_inserter(r),
+                   [transform](const auto &x) { return transform_path(transform, x); });
+    return r;
+}
 } // namespace horizon

src/util/clipper_util.hpp
@@ -3,4 +3,5 @@
 
 namespace horizon {
 ClipperLib::Path transform_path(const class Placement &transform, const ClipperLib::Path &path);
-}
+ClipperLib::Paths transform_paths(const Placement &transform, const ClipperLib::Paths &paths);
+} // namespace horizon