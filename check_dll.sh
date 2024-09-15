6345389a34c245c425d64ae73e5b1ac953b3ab2e
Lukas K. <lu@0x83.eu>
2024-09-15 02:31:51 +0200

    fix check_dll script
    binaries now are in bin

--- check_dll.sh
+++ check_dll.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-MISSING=$(ldd dist/horizon/horizon-eda.exe dist/horizon/horizon-imp.exe | grep -vi windows | grep -vi dist/horizon | grep -v "???")
+MISSING=$(ldd dist/horizon/bin/horizon-eda.exe dist/horizon/bin/horizon-imp.exe | grep -vi windows | grep -vi dist/horizon | grep -v "???")
 if [ -z "$MISSING" ]
 then
   echo "No missing DLLs"
