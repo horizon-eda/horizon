2fc58704d491d2959f5c7c28d8e5de3cad177dbf
Lukas K. <lu@0x83.eu>
2025-06-01 14:17:06 +0200

    version 2.6.91

--- scripts/CHANGELOG.md.in
+++ scripts/CHANGELOG.md.in
@@ -1,4 +1,4 @@
-# Version 2.6.90
+# Version 2.6.91
 
 ## New Features
 
--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 major = 2
 minor = 6
-micro = 90
+micro = 91
 name = "Luna"
 
 string = ".".join(str(x) for x in (major, minor, micro))
