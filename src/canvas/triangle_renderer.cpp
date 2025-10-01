horizon-eda/horizon

commit 8d97d0a23d5ddc1f30646a90ed02c3cb6c136b16

imp board: hide airwires if layer isn't visible/shadowed
also don't shadow airwires on the work layer

changelog: Enhancements/Board Editor: hide airwires on
invisible/shadowed layer, don't shadow visible airwires

fixes #808

src/canvas/triangle_renderer.cpp
@@ -303,6 +303,8 @@ void TriangleRenderer::render_layer_batch(int layer, HighlightMode highlight_mod
             auto lc = ca.get_layer_color(ca.layer_provider.get_color_layer(layer));
             buf.colors[static_cast<int>(ColorP::AIRWIRE_ROUTER)] =
                     gl_array_from_color(ca.appearance.colors.at(ColorP::AIRWIRE_ROUTER));
+            buf.colors[static_cast<int>(ColorP::AIRWIRE)] =
+                    gl_array_from_color(ca.appearance.colors.at(ColorP::AIRWIRE));
             buf.colors[static_cast<int>(ColorP::FROM_LAYER)] = apply_highlight(lc, highlight_mode, layer);
             buf.colors[static_cast<int>(ColorP::LAYER_HIGHLIGHT)] = apply_highlight(lc, highlight_mode, layer);
             buf.colors[static_cast<int>(ColorP::LAYER_HIGHLIGHT_LIGHTEN)] =

src/board/airwire.cpp
@@ -6,4 +6,12 @@ void Airwire::update_refs(Board &brd)
     to.update_refs(brd);
     from.update_refs(brd);
 }
+
+LayerRange Airwire::get_layers() const
+{
+    auto l = to.get_layer();
+    l.merge(from.get_layer());
+    return l;
+}
+
 } // namespace horizon

src/board/airwire.hpp
@@ -10,5 +10,6 @@ public:
     Track::Connection from;
     Track::Connection to;
     void update_refs(class Board &brd);
+    LayerRange get_layers() const;
 };
 } // namespace horizon

src/imp/imp_board.cpp
@@ -50,6 +50,8 @@ void ImpBoard::canvas_update()
 void ImpBoard::update_airwire_annotation()
 {
     airwire_annotation->clear();
+    const auto work_layer = canvas->property_work_layer().get_value();
+    const auto work_layer_only = layer_box->property_layer_mode() != CanvasGL::LayerMode::AS_IS;
     for (const auto &[net, airwires] : core_board.get_board()->airwires) {
         if (!airwire_filter_window->airwire_is_visible(net))
             continue;
@@ -60,6 +62,12 @@ void ImpBoard::update_airwire_annotation()
         if (core_board.tool_is_active() && !highlight && highlights.size())
             continue;
         for (const auto &airwire : airwires) {
+            const auto layers = airwire.get_layers();
+            if (work_layer_only && !layers.overlaps(work_layer))
+                continue;
+            if (!canvas->layer_is_visible(layers))
+                continue;
+
             airwire_annotation->draw_line(airwire.from.get_position(), airwire.to.get_position(), ColorP::AIRWIRE, 0,
                                           highlight, color2);
         }
@@ -887,6 +895,10 @@ void ImpBoard::construct()
 
         airwire_filter_window->set_only(nets);
     });
+
+    layer_box->property_layer_mode().signal_changed().connect([this] { update_airwire_annotation(); });
+    canvas->property_work_layer().signal_changed().connect([this] { update_airwire_annotation(); });
+    layer_box->signal_set_layer_display().connect([this](auto, auto) { update_airwire_annotation(); });
 
     connect_action(ActionID::OPEN_DATASHEET, [this](const auto &a) {
         const auto x = sel_find_exactly_one(canvas->get_selection(), ObjectType::BOARD_PACKAGE);

commit a5aa7380032c327b850677a72099303d6a34ee1f

imp board: fix highlighting airwires
got broken by 8d97d0a23d5ddc1f30646a90ed02c3cb6c136b16

src/canvas/triangle_renderer.cpp
@@ -179,7 +179,8 @@ static std::array<float, 4> operator*(const std::array<float, 4> &a, float b)
     return {a.at(0) * b, a.at(1) * b, a.at(2) * b, a.at(3)};
 }
 
-std::array<float, 4> TriangleRenderer::apply_highlight(const Color &icolor, HighlightMode mode, int layer) const
+std::array<float, 4> TriangleRenderer::apply_highlight(const Color &icolor, ColorP colorp, HighlightMode mode,
+                                                       int layer) const
 {
     const std::array<float, 4> color = gl_array_from_color(icolor);
     if (layer >= 20000 && layer < 30000) { // is annotation
@@ -187,7 +188,7 @@ std::array<float, 4> TriangleRenderer::apply_highlight(const Color &icolor, High
             return color;
     }
     if (ca.layer_mode == CanvasGL::LayerMode::SHADOW_OTHER) {
-        if (layer == ca.work_layer || ca.is_overlay_layer(layer, ca.work_layer)) {
+        if (layer == ca.work_layer || ca.is_overlay_layer(layer, ca.work_layer) || colorp == ColorP::AIRWIRE) {
             // it's okay continue as usual
         }
         else {
@@ -300,19 +301,19 @@ void TriangleRenderer::render_layer_batch(int layer, HighlightMode highlight_mod
             for (size_t i = 0; i < buf.colors.size(); i++) {
                 auto k = static_cast<ColorP>(i);
                 if (ca.appearance.colors.count(k))
-                    buf.colors[i] = apply_highlight(ca.appearance.colors.at(k), highlight_mode, layer);
+                    buf.colors[i] = apply_highlight(ca.appearance.colors.at(k), k, highlight_mode, layer);
             }
             auto lc = ca.get_layer_color(ca.layer_provider.get_color_layer(layer));
             buf.colors[static_cast<int>(ColorP::AIRWIRE_ROUTER)] =
                     gl_array_from_color(ca.appearance.colors.at(ColorP::AIRWIRE_ROUTER));
-            buf.colors[static_cast<int>(ColorP::AIRWIRE)] =
-                    gl_array_from_color(ca.appearance.colors.at(ColorP::AIRWIRE));
-            buf.colors[static_cast<int>(ColorP::FROM_LAYER)] = apply_highlight(lc, highlight_mode, layer);
-            buf.colors[static_cast<int>(ColorP::LAYER_HIGHLIGHT)] = apply_highlight(lc, highlight_mode, layer);
+            buf.colors[static_cast<int>(ColorP::FROM_LAYER)] =
+                    apply_highlight(lc, ColorP::FROM_LAYER, highlight_mode, layer);
+            buf.colors[static_cast<int>(ColorP::LAYER_HIGHLIGHT)] =
+                    apply_highlight(lc, ColorP::FROM_LAYER, highlight_mode, layer);
             buf.colors[static_cast<int>(ColorP::LAYER_HIGHLIGHT_LIGHTEN)] =
                     gl_array_from_color(lc) + ca.appearance.highlight_lighten;
             for (size_t i = 0; i < std::min(buf.colors2.size(), ca.colors2.size()); i++) {
-                buf.colors2[i] = apply_highlight(ca.colors2[i].to_color(), highlight_mode, layer);
+                buf.colors2[i] = apply_highlight(ca.colors2[i].to_color(), ColorP::FROM_LAYER, highlight_mode, layer);
             }
 
             if (ld.mode == LayerDisplay::Mode::FILL_ONLY || (key.stencil && use_stencil))

src/canvas/triangle_renderer.hpp
@@ -85,7 +85,7 @@ private:
     void render_layer_batch(int layer, HighlightMode highlight_mode, bool ignore_flip, const Batch &batch,
                             bool use_stencil, bool stencil_mode);
     void render_annotations(bool top);
-    std::array<float, 4> apply_highlight(const class Color &color, HighlightMode mode, int layer) const;
+    std::array<float, 4> apply_highlight(const class Color &color, ColorP colorp, HighlightMode mode, int layer) const;
     int stencil = 0;
 
     std::array<std::vector<unsigned int>, BatchKey::hash_max + 1> type_indices;