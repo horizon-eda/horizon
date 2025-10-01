horizon-eda/horizon

commit 90bf26e2117b4140af30eec3654a8f3cc633b993

pdf export: fix backward text
changelog: Bugfixes/Editor: PDF export: correctly render backwards text

src/export_pdf/canvas_pdf.cpp
@@ -10,9 +10,17 @@
 
 namespace horizon {
 
+// one pt is 1/72 inch
+static constexpr double nm_to_pt_factor = 72. / (2.54_mm);
+
 double to_pt(double x_nm)
 {
-    return x_nm * .000002834645669291339;
+    return x_nm * nm_to_pt_factor;
+}
+
+static double from_pt(double x_pt)
+{
+    return x_pt / nm_to_pt_factor;
 }
 
 CanvasPDF::CanvasPDF(PoDoFo::PdfPainter &p, PoDoFo::PdfFont &f, const PDFExportSettings &s)
@@ -92,7 +100,7 @@ void CanvasPDF::img_draw_text(const Coordf &p, float size, const std::string &rt
     painter.TextState.SetFont(font, to_pt(size) * 1.6);
     while (std::getline(ss, line, '\n')) {
         line = TextData::trim(line);
-        int64_t line_width = font.GetStringLength(line.c_str(), painter.TextState);
+        const int64_t line_width = from_pt(font.GetStringLength(line.c_str(), painter.TextState));
 
         Placement tf;
         tf.shift.x = p.x;

commit 477b2641a4052214ae5e73e5e0f7be36f1444999

pdf export: one inch is 25.4mm, not 2.54m...
changelog: Bugfixes/Editors: export PDFs at x1 scale instead of x10

got broken in 90bf26e2117b4140af30eec3654a8f3cc633b993

src/export_pdf/canvas_pdf.cpp
@@ -11,7 +11,7 @@
 namespace horizon {
 
 // one pt is 1/72 inch
-static constexpr double nm_to_pt_factor = 72. / (2.54_mm);
+static constexpr double nm_to_pt_factor = 72. / (25.4_mm);
 
 double to_pt(double x_nm)
 {