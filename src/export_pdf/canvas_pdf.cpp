horizon-eda/horizon

commit 8c8e8f4b336032a1df6f65694698dd5f07beb1e2

board pdf export: support dimensions
changelog: Enhancements/Board Editor: support dimensions in PDF export

src/export_pdf/canvas_pdf.cpp
@@ -27,6 +27,7 @@ CanvasPDF::CanvasPDF(PoDoFo::PdfPainter &p, PoDoFo::PdfFont &f, const PDFExportS
     : Canvas::Canvas(), painter(p), font(f), settings(s), metrics(font.GetMetrics())
 {
     img_mode = true;
+    img_draw_dimensions = s.layers.at(PDFExportSettings::DIMENSIONS_LAYER).enabled;
     Appearance apperarance;
     layer_colors = apperarance.layer_colors;
     path.Reset();
@@ -50,6 +51,8 @@ Color CanvasPDF::get_pdf_layer_color(int layer) const
 
 void CanvasPDF::img_line(const Coordi &p0, const Coordi &p1, const uint64_t width, int layer, bool tr)
 {
+    if (triangle_type_current == TriangleInfo::Type::DIMENSION)
+        layer = PDFExportSettings::DIMENSIONS_LAYER;
     if (!pdf_layer_visible(layer))
         return;
 
@@ -70,6 +73,8 @@ void CanvasPDF::img_draw_text(const Coordf &p, float size, const std::string &rt
                               TextOrigin origin, int layer, uint64_t width, TextData::Font tfont, bool center,
                               bool mirror)
 {
+    if (triangle_type_current == TriangleInfo::Type::DIMENSION)
+        layer = PDFExportSettings::DIMENSIONS_LAYER;
     if (!settings.include_text)
         return;
     if (!pdf_layer_visible(layer))

src/board/board.cpp
@@ -696,7 +696,7 @@ void Board::update_pdf_export_settings(PDFExportSettings &settings)
     auto layers_from_board = get_layers();
     // remove layers not on board
     map_erase_if(settings.layers, [&layers_from_board](const auto &it) {
-        return it.first != PDFExportSettings::HOLES_LAYER && layers_from_board.count(it.first) == 0;
+        return !PDFExportSettings::is_special_layer(it.first) && layers_from_board.count(it.first) == 0;
     });
 
     // add new layers
@@ -708,6 +708,7 @@ void Board::update_pdf_export_settings(PDFExportSettings &settings)
 
     // add holes layer
     add_layer(PDFExportSettings::HOLES_LAYER, false);
+    add_layer(PDFExportSettings::DIMENSIONS_LAYER, false);
 
     add_layer(BoardLayers::OUTLINE_NOTES);
     add_layer(BoardLayers::L_OUTLINE);

src/canvas/canvas.hpp
@@ -236,6 +236,7 @@ protected:
     }
     bool img_mode = false;
     bool img_auto_line = false;
+    bool img_draw_dimensions = false;
 
     Placement transform;
     void transform_save();

src/canvas/render.cpp
@@ -1571,9 +1571,10 @@ void Canvas::render(const BoardHole &hole, bool interactive)
 
 void Canvas::render(const class Dimension &dim)
 {
-    if (img_mode)
+    if (img_mode && !img_draw_dimensions)
         return;
 
+    img_auto_line = img_mode;
     triangle_type_current = TriangleInfo::Type::DIMENSION;
     Coordd p0 = dim.p0;
     Coordd p1 = dim.p1;
@@ -1587,14 +1588,7 @@ void Canvas::render(const class Dimension &dim)
 
     const Coordd v = p1 - p0;
     const auto vn = v.normalize();
-    Coordd w = Coordd(-v.y, v.x);
-    if (dim.mode == Dimension::Mode::HORIZONTAL) {
-        w.y = std::abs(w.y);
-    }
-    else if (dim.mode == Dimension::Mode::VERTICAL) {
-        w.x = std::abs(w.x);
-    }
-    const auto wn = w.normalize();
+    const auto wn = dim.get_normalized_offset();
 
     ColorP co = ColorP::DIMENSION;
 
@@ -1667,6 +1661,7 @@ void Canvas::render(const class Dimension &dim)
     targets.emplace_back(dim.uuid, ObjectType::DIMENSION, dim.p0, 0);
     targets.emplace_back(dim.uuid, ObjectType::DIMENSION, dim.p1, 1);
     triangle_type_current = TriangleInfo::Type::NONE;
+    img_auto_line = false;
 }
 
 void Canvas::render(const Board &brd, bool interactive, PanelMode mode, OutlineMode outline_mode)

src/common/dimension.cpp
@@ -1,6 +1,8 @@
 #include "dimension.hpp"
 #include "lut.hpp"
 #include "nlohmann/json.hpp"
+#include "util/bbox_accumulator.hpp"
+#include "util/geom_util.hpp"
 
 namespace horizon {
 
@@ -65,4 +67,37 @@ int64_t Dimension::get_length() const
         return 0;
     }
 }
+
+std::pair<Coordi, Coordi> Dimension::get_bbox() const
+{
+    BBoxAccumulator<int64_t> acc;
+    acc.accumulate(p0);
+    acc.accumulate(p1);
+    auto wn = get_normalized_offset();
+    acc.accumulate(p0 + (wn * (label_distance + sgn(label_distance) * (double)label_size / 2.)).to_coordi());
+    acc.accumulate(p1 + (wn * (label_distance + sgn(label_distance) * (double)label_size / 2.)).to_coordi());
+    return acc.get();
+}
+
+Coordd Dimension::get_normalized_offset() const
+{
+    Coordd p0x = p0;
+    Coordd p1x = p1;
+    if (mode == Dimension::Mode::HORIZONTAL) {
+        p1x = Coordd(p1x.x, p0x.y);
+    }
+    else if (mode == Dimension::Mode::VERTICAL) {
+        p1x = Coordd(p0x.x, p1x.y);
+    }
+    const Coordd v = p1x - p0x;
+    Coordd w = Coordd(-v.y, v.x);
+    if (mode == Mode::HORIZONTAL) {
+        w.y = std::abs(w.y);
+    }
+    else if (mode == Mode::VERTICAL) {
+        w.x = std::abs(w.x);
+    }
+    return w.normalize();
+}
+
 } // namespace horizon

src/common/dimension.hpp
@@ -22,6 +22,8 @@ public:
 
     int64_t project(const Coordi &c) const;
     int64_t get_length() const;
+    std::pair<Coordi, Coordi> get_bbox() const;
+    Coordd get_normalized_offset() const;
 
     json serialize() const;
 };

src/common/pdf_export_settings.cpp
@@ -50,6 +50,17 @@ PDFExportSettings::PDFExportSettings()
 {
 }
 
+bool PDFExportSettings::is_special_layer(int layer)
+{
+    switch (layer) {
+    case HOLES_LAYER:
+    case DIMENSIONS_LAYER:
+        return true;
+    default:
+        return false;
+    }
+}
+
 json PDFExportSettings::serialize_schematic() const
 {
     json j;

src/common/pdf_export_settings.hpp
@@ -24,7 +24,8 @@ public:
     bool set_holes_size = false;
     uint64_t holes_diameter = 0;
 
-    enum { HOLES_LAYER = 10000 };
+    enum { HOLES_LAYER = 10'000, DIMENSIONS_LAYER = 10'001 };
+    static bool is_special_layer(int layer);
 
     class Layer {
     public:

commit bf2ca14e1bd1cb9ec29c242dc8f1dacdf87ae976

fix schematic pdf export
got broken in 8c8e8f4b336032a1df6f65694698dd5f07beb1e2

fixes #843

src/export_pdf/canvas_pdf.cpp
@@ -27,7 +27,8 @@ CanvasPDF::CanvasPDF(PoDoFo::PdfPainter &p, PoDoFo::PdfFont &f, const PDFExportS
     : Canvas::Canvas(), painter(p), font(f), settings(s), metrics(font.GetMetrics())
 {
     img_mode = true;
-    img_draw_dimensions = s.layers.at(PDFExportSettings::DIMENSIONS_LAYER).enabled;
+    if (s.layers.count(PDFExportSettings::DIMENSIONS_LAYER))
+        img_draw_dimensions = s.layers.at(PDFExportSettings::DIMENSIONS_LAYER).enabled;
     Appearance apperarance;
     layer_colors = apperarance.layer_colors;
     path.Reset();